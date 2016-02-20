5375c08ecc9d851afdf149bda10a89681f6068cf
Harlan T Wood <dev+harlan@comakery.com>
2016-02-19 19:24:56 -0800

    do not globally config slack for one instance

--- config/initializers/slack.rb
+++ config/initializers/slack.rb
@@ -1,7 +1,3 @@
-Slack.configure do |config|
-  config.token = ENV['SLACK_API_TOKEN']
-end
-
 Slack::RealTime.configure do |config|
   config.concurrency = Slack::RealTime::Concurrency::Celluloid
 end
