b0654469298ef3b653791552510dd71469464594
Glenn Jahnke <glenn@citizencode.io>
2016-02-25 13:01:13 -0800

    fix borked css name change

--- app/views/projects/base.rb
+++ app/views/projects/base.rb
@@ -15,7 +15,7 @@ class Views::Projects::Base < Views::Base
   def project_block(project)
     row(class: "project", id: "project-#{project.to_param}") {
       column("small-4") {
-        img(src: attachment_url(project, :image, :fill, 100, 100), class: "small-margin", width: 100, height: 100)
+        img(src: attachment_url(project, :image, :fill, 100, 100), class: "margin-small", width: 100, height: 100)
       }
       column("small-8") {
         div(class: "margin-small") {
