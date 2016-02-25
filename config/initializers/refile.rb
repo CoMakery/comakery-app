7023d72752f96ffe18b246430057c00a358c7bd9
Glenn Jahnke <glenn@citizencode.io>
2016-02-25 11:33:42 -0800

    sync cache and store maximum file size for refile image storage

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -7,6 +7,6 @@ if !Rails.env.development? && !Rails.env.test?
       region: ENV['REFILE_S3_REGION'],
       bucket: ENV['REFILE_S3_BUCKET'],
   }
-  Refile.cache = Refile::S3.new(prefix: "cache", max_size: 5.megabytes, **aws)
-  Refile.store = Refile::S3.new(prefix: "store", **aws)
+  Refile.cache = Refile::S3.new(prefix: "cache", max_size: 10.megabytes, **aws)
+  Refile.store = Refile::S3.new(prefix: "store", max_size: 10.megabytes, **aws)
 end
