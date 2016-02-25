2e67429ce0553f2717d4de3d24f707b2be0cda05
Harlan T Wood <dev+harlan@comakery.com>
2016-02-25 15:35:30 -0800

    allow email from multiple places in the auth hash

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -12,7 +12,7 @@ class Authentication < ActiveRecord::Base
     slack_team_id = auth_hash.dig('info', 'team_id')
     slack_team_name = auth_hash.dig('info', 'team')
     slack_token = auth_hash.dig('credentials', 'token')
-    email_address = auth_hash.dig('extra', 'user_info', 'user', 'profile', 'email')
+    email_address = auth_hash.dig('info', 'email').presence || auth_hash.dig('extra', 'user_info', 'user', 'profile', 'email')
 
     unless provider && email_address && name && slack_team_id && slack_team_name && slack_user_id && slack_token
       raise MissingAuthParamException.new({provider: provider,
