d3d04e814bad9c37274cbe089c1f6fa014dffb48
Glenn Jahnke and Harlan T Wood <dev+glenn+harlan@comakery.com>
2016-03-02 12:59:23 -0800

    default vcr mode in env to :none

--- spec/support/vcr_setup.rb
+++ spec/support/vcr_setup.rb
@@ -9,7 +9,7 @@ RSpec.configure do |config|
   config.around(:each, :vcr) do |example|
     name = example.metadata[:full_description].downcase.gsub(/\W+/, "_").split("_", 2).join("/")
 
-    vcr_mode = ENV["VCR_MODE"].to_sym
+    vcr_mode = (ENV["VCR_MODE"] || :none).to_sym
     raise "bang" unless vcr_mode.in?([:all, :none, :new_episodes])
 
     VCR.use_cassette(name, :record => vcr_mode) do
