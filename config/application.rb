60ea3fe722a4d4cb76dee7dce41d571ffca4bbb4
Glenn Jahnke <dev+glenn@comakery.com>
2016-03-07 11:49:07 -0800

    tighten up CORS

--- config/application.rb
+++ config/application.rb
@@ -48,12 +48,7 @@ module Swarmbot
     cloudfront_host = ENV['CLOUDFRONT_HOST']
     if cloudfront_host.present?
       config.action_controller.asset_host = cloudfront_host
-      config.action_dispatch.default_headers.merge!({
-                                                        # 'Access-Control-Allow-Origin' => "http://#{cloudfront_host}",
-                                                        # 'Access-Control-Request-Method' => 'GET'
-                                                        'Access-Control-Allow-Origin' => "*",
-                                                        'Access-Control-Request-Method' => "*"
-                                                    })
+      config.font_assets.origin = "http://#{cloudfront_host}"
     end
   end
 end
