aaa18aefd6176fb009a471fea7cefd3578f69eec
Glenn Jahnke <dev+glenn@comakery.com>
2016-03-09 19:02:34 -0800

    added useless search bar

--- app/views/shared/navigation.rb
+++ app/views/shared/navigation.rb
@@ -1,7 +1,29 @@
 class Views::Shared::Navigation < Views::Base
   def content
+    content_for :js do
+      text(<<-JAVASCRIPT.html_safe)
+        $(function() {
+          $(".project-search a").on("click", function() {
+            window.location = "http://www.google.com"
+          })
+        });
+      JAVASCRIPT
+    end
+
     section(class: "top-bar-section") {
       ul(class: "right") {
+        li(class: "has-form") {
+          div(class: "row collapse project-search") {
+            form_tag("https://www.google.com", method: "GET") {
+              div(class: "large-8 small-9 columns collapse") {
+                input(name: "q", type: "text", placeholder: "Search Projects")
+              }
+              div(class: "large-4 small-3 columns collapse") {
+                button_tag("Search", class: "button expand")
+              }
+            }
+          }
+        }
         li(class: "slack-instance") {
           if current_account&.slack_auth
             div(class: "top-bar-text") {
