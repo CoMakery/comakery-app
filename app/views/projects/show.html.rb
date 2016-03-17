2403c2bc769ae93654d9251abc945b8288ba7109
Glenn Jahnke <dev+glenn@comakery.com>
2016-03-17 15:42:31 -0700

    fix double box on award box

--- app/views/projects/show.html.rb
+++ app/views/projects/show.html.rb
@@ -74,55 +74,54 @@ class Views::Projects::Show < Views::Base
         }
       }
     }
-    row(class:"project-body") {
+    row(class: "project-body") {
       column("small-5 award-send") {
         form_for [project, award] do |f|
-          row() {
-            div(class: "award-send") {
-              h3 "Send awards"
-              project.award_types.each do |award_type|
-                row(class: "award-type-row") {
-                  column("small-12") {
-                    with_errors(project, :account_id) {
-                      label {
-                        f.radio_button(:award_type_id, award_type.to_param) if awardable_types.include?(award_type)
-                        span(award_type.name)
-                        text " (#{award_type.amount})"
-                        text " (Community Awardable)" if award_type.community_awardable?
-                      }
-                    }
-                  }
-                }
-              end
-              if awardable_types.any?{|awardable_type|awardable_type.community_awardable?} || project.owner_account == current_user
-                row {
-                  column("small-8") {
+          row(class: "award-types") {
+
+            h3 "Send awards"
+            project.award_types.each do |award_type|
+              row(class: "award-type-row") {
+                column("small-12") {
+                  with_errors(project, :account_id) {
                     label {
-                      text "User"
-                      options = capture do
-                        options_for_select([[nil, nil]].concat(awardable_accounts||[]))
-                      end
-                      select_tag "award[slack_user_id]", options, html: {id: "award_slack_user_id"}
+                      f.radio_button(:award_type_id, award_type.to_param) if current_user && awardable_types.include?(award_type)
+                      span(award_type.name)
+                      text " (#{award_type.amount})"
+                      text " (Community Awardable)" if award_type.community_awardable?
                     }
                   }
                 }
-                row {
-                  column("small-12") {
-                    with_errors(project, :description) {
-                      label {
-                        text "Description"
-                        f.text_area(:description)
-                      }
+              }
+            end
+            if awardable_types.any? { |awardable_type| awardable_type.community_awardable? } || project.owner_account == current_user
+              row {
+                column("small-8") {
+                  label {
+                    text "User"
+                    options = capture do
+                      options_for_select([[nil, nil]].concat(awardable_accounts||[]))
+                    end
+                    select_tag "award[slack_user_id]", options, html: {id: "award_slack_user_id"}
+                  }
+                }
+              }
+              row {
+                column("small-12") {
+                  with_errors(project, :description) {
+                    label {
+                      text "Description"
+                      f.text_area(:description)
                     }
                   }
                 }
-                row {
-                  column("small-12") {
-                    f.submit("Send Award", class: buttonish(:tiny, :round))
-                  }
+              }
+              row {
+                column("small-12") {
+                  f.submit("Send Award", class: buttonish(:tiny, :round))
                 }
-              end
-            }
+              }
+            end
           }
         end
       }
