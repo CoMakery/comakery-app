bf2e3c7ac182123dba3d964bb21c39a3deff0c31
Glenn Jahnke <dev+glenn@comakery.com>
2016-03-22 18:09:43 -0700

    fix so showing top contributors by actual amount awarded; sort by amount awarded as well as created_at

--- app/interactors/top_contributors.rb
+++ app/interactors/top_contributors.rb
@@ -3,22 +3,25 @@ class TopContributors
 
   def call
     projects = context.projects
-    n = context.n
+    n = context.n || 5
 
-    contributors = projects.each_with_object({}) {|project, projects_to_contributors| projects_to_contributors[project] = top_contributors(project, n) }
+    contributors = projects.each_with_object({}) { |project, projects_to_contributors| projects_to_contributors[project] = top_contributors(project, n) }
 
     context.contributors = contributors
   end
 
   def top_contributors(project, n)
-    all_columns = Authentication.column_names.map{|column| "max(authentications.#{column}) as #{column}"}.join(",")
-    project.contributors.
-            select("#{all_columns}, sum(award_types.amount) as total_awarded").
-            group("authentications.id").
-            order("total_awarded desc").
-            limit(n).
-            to_a.
-            sort_by(&:total_awarded).
-            reverse
+    all_columns = Authentication.column_names.map { |column| "max(authentications.#{column}) as #{column}" }.join(", ")
+    Authentication.
+        select("#{all_columns}, sum(award_type_amount) as total_awarded, max(last_awarded_at) as last_awarded_at").
+        from(project.contributors.
+            select("#{all_columns}, count(awards.id) * award_types.amount as award_type_amount, max(awards.created_at) as last_awarded_at").
+            group("authentications.id, award_types.id"),
+            "authentications"
+        ).
+        group("authentications.id").
+        order("total_awarded desc, max(last_awarded_at) desc").
+        limit(n).
+        to_a
   end
 end
