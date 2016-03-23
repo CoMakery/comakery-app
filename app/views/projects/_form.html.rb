8c15244d0a806ed13c100e13af3a0c19b02045b5
Victoria Wang <violasong@gmail.com>
2016-03-23 11:18:22 -0700

    Initial styling for edit/create page

--- app/views/projects/_form.html.rb
+++ app/views/projects/_form.html.rb
@@ -6,60 +6,66 @@ module Views
       def content
         form_for project do |f|
           row {
-            with_errors(project, :title) {
-              label {
-                text "Title"
-                f.text_field :title
+            column("large-6 small-12") {
+              with_errors(project, :title) {
+                label {
+                  text "Title"
+                  f.text_field :title
+                }
+              }
+              with_errors(project, :description) {
+                label {
+                  text "Description"
+                  f.text_field :description
+                }
+              }
+              with_errors(project, :tracker) {
+                label {
+                  i(class: "fa fa-tasks")
+                  text " Project Tracker"
+                  f.text_field :tracker, placeholder: "https://pivotaltracker.com"
+                }
+              }
+              with_errors(project, :slack_channel) {
+                label {
+                  i(class: "fa fa-slack")
+                  text " Slack Channel"
+                  options = capture do
+                    options_for_select([[nil, nil]].concat(slack_channels), selected: project.slack_channel)
+                  end
+                  select_tag "project[slack_channel]", options, html: {id: "project_slack_channel"}
+                }
+              }
+              with_errors(project, :public) {
+                label {
+                  f.check_box :public
+                  text " Set project as public (display in CoMakery index)"
+                }
               }
             }
-            with_errors(project, :description) {
-              label {
-                text "Description"
-                f.text_field :description
-              }
-            }
-            with_errors(project, :tracker) {
-              label {
-                text "Project Tracker"
-                f.text_field :tracker, placeholder: "https://pivotaltracker.com"
-              }
-            }
-            with_errors(project, :slack_channel) {
-              label {
-                text "Slack Channel"
-                options = capture do
-                  options_for_select([[nil, nil]].concat(slack_channels), selected: project.slack_channel)
-                end
-                select_tag "project[slack_channel]", options, html: {id: "project_slack_channel"}
-              }
-            }
-            with_errors(project, :image) {
-              label {
-                text "Project Image (A large, roughly square image works best)"
-                text f.attachment_field(:image)
-              }
-              text attachment_image_tag(project, :image, class: "project-image")
-            }
-            with_errors(project, :public) {
-              label {
-                f.check_box :public
-                text " Set project as public (display in CoMakery index)"
+            column("large-6 small-12") {
+              with_errors(project, :image) {
+                label {
+                  text "Project Image (At least 450x400 px)"
+                  text f.attachment_field(:image)
+                }
+                text attachment_image_tag(project, :image, class: "project-image")
               }
             }
           }
 
           row {
-            column("small-4", class: "text-center") {
+            column("small-4", class: "") {
               text "Award Names"
             }
-            column("small-2", class: "text-center") {
+            column("small-2", class: "") {
               text "Coin Value"
             }
-            column("small-2", class: "text-center") {
+            column("small-2", class: "") {
               text "Community Awardable "
               question_tooltip("Check this box if you want people on your team to be able to award others. Otherwise only the project owner can send awards.")
             }
-            column("small-4", class: "text-center") {
+            column("small-4", class: "") {
             }
           }
           div(class: "award-types") {
@@ -86,7 +92,7 @@ module Views
                 }
                 column("small-4") {
                   if ff.object&.modifiable?
-                    a("×", href: "#", 'data-mark-and-hide': '.award-type-row')
+                    a("×", href: "#", 'data-mark-and-hide': '.award-type-row', class: "close")
                   else
                     text "(#{pluralize(ff.object.awards.count, "award")} sent)"
                   end
