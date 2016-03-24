229c1e359530d389d3162d79b8e587acabc449d2
Glenn Jahnke and Harlan T Wood <dev+glenn+harlan@comakery.com>
2016-03-24 10:45:58 -0700

    Revert "get rid of n-queries using has_one :slack_auth"
    This reverts commit 978c29f1374aa5e754af0462b93310ee112a120f.

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,8 +1,6 @@
 class Account < ActiveRecord::Base
   has_many :account_roles, dependent: :destroy
-  has_many :authentications, dependent: :destroy
-  has_one :slack_auth, -> { where(provider: "slack").order("updated_at desc").limit(1) }, class_name: Authentication
-  default_scope { includes(:slack_auth) }
+  has_many :authentications, -> { order(updated_at: :desc) }, dependent: :destroy
   has_many :account_roles, dependent: :destroy
   has_many :roles, through: :account_roles
 
@@ -21,9 +19,9 @@ class Account < ActiveRecord::Base
     @slack ||= Comakery::Slack.get(slack_auth.slack_token)
   end
 
-  # def slack_auth(**scope)
-  #   authentications.find_by(**scope.merge(provider: "slack"))
-  # end
+  def slack_auth(**scope)
+    authentications.find_by(**scope.merge(provider: "slack"))
+  end
 
   def send_award_notifications(**args)
     slack.send_award_notifications(**args)
