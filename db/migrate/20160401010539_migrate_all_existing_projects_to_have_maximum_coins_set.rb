d3cabe7acd91023d4add0143b995d03ed7bde95f
Glenn Jahnke <dev+glenn@comakery.com>
2016-03-31 18:06:47 -0700

    migrate all existing projects to have maximum coins set to something above 0

--- db/migrate/20160401010539_migrate_all_existing_projects_to_have_maximum_coins_set.rb
+++ db/migrate/20160401010539_migrate_all_existing_projects_to_have_maximum_coins_set.rb
@@ -1,5 +1,8 @@
 class MigrateAllExistingProjectsToHaveMaximumCoinsSet < ActiveRecord::Migration
-  def change
-    Project.update_all(maximum_coins: 10_000_000)
+    Project.where(maximum_coins: 0).update_all(maximum_coins: 10_000_000)
+  end
+
+  def down
+    puts "no-op"
   end
 end
