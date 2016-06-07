69d4072dc18b982bb42d3f9c71bde74f625b0dcd
Harlan T Wood and Noah Thorp <dev+harlan+noah@comakery.com>
2016-06-06 17:16:12 -0700

    refactor ethereum api module

--- app/lib/comakery/ethereum.rb
+++ app/lib/comakery/ethereum.rb
@@ -5,42 +5,28 @@ class Comakery::Ethereum
     TIMEOUT = 10.minutes
 
     def token_contract args
-      ethereum_bridge = ENV['ETHEREUM_BRIDGE'].presence
-      return if ethereum_bridge.nil? && Comakery::Application.config.allow_missing_ethereum_bridge
-      raise("please set env var ETHEREUM_BRIDGE") unless ethereum_bridge
-
-      url = URI.join ethereum_bridge, "project"
-      headers = { 'Content-Type' => 'application/json', 'Accept' => 'application/json'}
-
-      response = HTTParty.post url, body: args.to_json, headers: headers, timeout: TIMEOUT
-
-      begin
-        return response.parsed_response.fetch('contractAddress')
-      rescue => error
-        message = "Error received: #{response.parsed_response.inspect}
-          From request to: #{url}
-          with params: #{JSON.pretty_generate args}
-        "
-        Airbrake.notify(Exception.new(message))
-        nil
-      end
+      call_ethereum_bridge('project', args,'contractAddress')
     end
 
     def token_issue args
+      call_ethereum_bridge('token_issue', args,'transactionId')
+    end
+
+    def call_ethereum_bridge(path, url_args, response_key)
       ethereum_bridge = ENV['ETHEREUM_BRIDGE'].presence
       return if ethereum_bridge.nil? && Comakery::Application.config.allow_missing_ethereum_bridge
       raise("please set env var ETHEREUM_BRIDGE") unless ethereum_bridge
 
-      url = URI.join ethereum_bridge, "token_issue"
+      url = URI.join ethereum_bridge, path
       headers = { 'Content-Type' => 'application/json', 'Accept' => 'application/json'}
-      response = HTTParty.post url, body: args.to_json, headers: headers, timeout: TIMEOUT
+      response = HTTParty.post url, body: url_args.to_json, headers: headers, timeout: TIMEOUT
 
       begin
-        return response.parsed_response.fetch('transactionId')
+        response.parsed_response.fetch(response_key)
       rescue => error
         message = "Error received: #{response.parsed_response.inspect}
           From request to: #{url}
-          with params: #{JSON.pretty_generate args}
+          with params: #{JSON.pretty_generate url_args}
         "
         Airbrake.notify(Exception.new(message))
         nil
