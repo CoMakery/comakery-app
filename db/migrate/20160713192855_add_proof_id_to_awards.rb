5b9f47def1c1c644deabdf7deb88416508075259
Harlan T Wood <dev+harlan@comakery.com>
2016-07-13 14:48:55 -0700

    make migration cheaper for production: on demo it took 266 sec for 10k award rows

--- db/migrate/20160713192855_add_proof_id_to_awards.rb
+++ db/migrate/20160713192855_add_proof_id_to_awards.rb
@@ -3,7 +3,7 @@ class AddProofIdToAwards < ActiveRecord::Migration
     add_column :awards, :proof_id, :text
 
     Award.where(proof_id: nil).each do |award|
-      award.update proof_id: SecureRandom.base58(44)
+      award.update proof_id: SecureRandom.base58(22)
     end
 
     change_column :awards, :proof_id, :text, :null => false
