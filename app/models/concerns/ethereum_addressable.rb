cae8553a8c9e2c85a47b408ae099496b8b66cf8e
Harlan T Wood <dev+harlan@comakery.com>
2016-08-08 17:25:41 -0700

    better error message

--- app/models/concerns/ethereum_addressable.rb
+++ app/models/concerns/ethereum_addressable.rb
@@ -4,7 +4,7 @@ module EthereumAddressable
   class EthereumAddressValidator < ActiveModel::EachValidator
     def validate_each(record, attribute, value)
       return if value.blank?
-      length = options[:length] || raise(ArgumentError.new("length is required"))
+      length = options[:length] || raise(ArgumentError.new("length is required, for example: `validates :my_field, ethereum_address: {length: 40}`"))
       if value !~ /\A0x[0-9a-fA-F]{#{length}}\z/
         message = options[:message] || "should start with '0x', followed by a #{length} character ethereum address"
         record.errors.add attribute, message
