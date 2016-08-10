fb02c854a222c714e130177ef075c1e2e160379a
Harlan T Wood <dev+harlan@comakery.com>
2016-08-10 15:25:30 -0700

    use only ENV['APP_NAME'], and require it to be set

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,9 +1,11 @@
 def configure_airbrake
 
-  if ENV['AIRBRAKE_API_KEY'].blank? || ENV['AIRBRAKE_PROJECT_ID'].blank?
+  if ENV['AIRBRAKE_API_KEY'].blank? || ENV['AIRBRAKE_PROJECT_ID'].blank? ||
+          ENV['APP_NAME'].blank?
     Rails.logger.error '!' * 50
     Rails.logger.error "Error reporting is not set up! " \
-      "Please set ENV['AIRBRAKE_API_KEY'] and ENV['AIRBRAKE_PROJECT_ID'] " \
+      "Please set ENV['AIRBRAKE_API_KEY'] and ENV['AIRBRAKE_PROJECT_ID'] and " \
+      "ENV['APP_NAME'] " \
       "See: https://github.com/airbrake/airbrake-ruby#project_id--project_key"
     return
   end
@@ -14,8 +16,7 @@ def configure_airbrake
 
     c.root_directory = Rails.root
     c.logger = Rails.logger
-    c.environment = ENV['APP_NAME'] || ENV['RAILS_ENV'] || ENV['RACK_ENV'] || Rails.env
-    c.ignore_environments = %w(test)
+    c.environment = ENV['APP_NAME']
 
     # A list of parameters that should be filtered out of what is sent to
     # Airbrake. By default, all "password" attributes will have their contents
