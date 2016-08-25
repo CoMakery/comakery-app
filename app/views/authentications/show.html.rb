62e53e4996888da0b660e922ae4f738ac5fabb41
Kyle Leimetter <thinkin3d@gmail.com>
2016-08-25 12:59:36 -0700

    logo svg no border whitespace, account awards ui polishing

--- app/views/authentications/show.html.rb
+++ app/views/authentications/show.html.rb
@@ -51,7 +51,7 @@ class Views::Authentications::Show < Views::Base
     hr
 
     awards.group_by { |award| award.award_type.project_id }.each do |(_, awards_for_project)|
-      h3 "#{awards_for_project.first.award_type.project.title} awards"
+      h3 "#{awards_for_project.first.award_type.project.title} awards", class: "awards-title"
       render partial: "shared/awards", locals: {awards: AwardDecorator.decorate_collection(awards_for_project), show_recipient: false}
     end
   end
