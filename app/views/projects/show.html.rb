082b440bca00e0c21361918b028fdbd8079645b0
Harlan T Wood <dev+harlan@comakery.com>
2016-08-30 22:47:37 -0700

    Revert "add project name to HTML title tag"
    This reverts commit 43cfd7d023eae926fc098b43ab1930aef610057b.

--- app/views/projects/show.html.rb
+++ app/views/projects/show.html.rb
@@ -17,10 +17,6 @@ class Views::Projects::Show < Views::Projects::Base
   end
 
   def content
-    content_for(:title) {
-      "#{project.title} - #{Rails.application.config.project_name}"
-    }
-
     if award_data[:contributions].present?
       content_for :js do
         make_charts
