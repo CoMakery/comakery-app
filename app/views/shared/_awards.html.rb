7f43ac5ab34c37acc99a4a4b13c0fd76e04d7aa5
Harlan T Wood <dev+harlan@comakery.com>
2016-09-07 18:03:41 -0700

    scroll awards table when on narrow screen

--- app/views/shared/_awards.html.rb
+++ app/views/shared/_awards.html.rb
@@ -2,67 +2,70 @@ class Views::Shared::Awards < Views::Base
   needs :project, :awards, :show_recipient, :current_account
 
   def content
-    div(class: "award-rows") {
-      row(class: "header-row") {
-        column("small-1") { div(class: "header") { text "Tokens Issued" } }
-        column("small-2") { div(class: "header") { text "Date" } }
-        if show_recipient
-          column("small-2") { div(class: "header") { text "Recipient" } }
-        end
-        column("small-3") { div(class: "header") { text "Contribution" } }
-        column("small-2") { div(class: "header") { text "Authorized By" } }
-        if project.ethereum_enabled
-          column("small-2") { div(class: "header blockchain-address") {
-            text "Blockchain Transaction" }
-          }
-        end
-      }
-      awards.sort_by(&:created_at).reverse.each do |award|
-        row(class: "award-row") {
-          column("small-1") {
-            text number_with_delimiter(award.award_type.amount, :delimiter => ',')
-          }
-          column("small-2") {
-            text raw award.created_at.strftime("%b %d, %Y").gsub(' ', '&nbsp;')
-          }
+    div(class: "table-scroll") {
+      table(class: "award-rows") {
+        tr(class: "header-row") {
+          th(class: "small-1") { div(class: "header") { text "Tokens Issued" } }
+          th(class: "small-2") { div(class: "header") { text "Date" } }
           if show_recipient
-            column("small-2") {
-              img(src: award.authentication.slack_icon, class: "icon avatar-img")
-              text " " + award.recipient_display_name
+            th(class: "small-2") { div(class: "header") { text "Recipient" } }
+          end
+          th(class: "small-3") { div(class: "header") { text "Contribution" } }
+          th(class: "small-2") { div(class: "header") { text "Authorized By" } }
+          if project.ethereum_enabled
+            th(class: "small-2") { div(class: "header blockchain-address") {
+              text "Blockchain Transaction" }
             }
           end
-          column("small-3") {
-            if award.proof_link
-              link_to award.proof_id_short, award.proof_link, target: '_blank'
-            else
-              span award.proof_id_short
-            end
-            br
-            strong "#{award.award_type.name}"
-            text raw ": #{markdown_to_html award.description}" if award.description.present?
-          }
-          column("small-2") {
-            if award.issuer_slack_icon
-              img(src: award.issuer_slack_icon, class: "icon avatar-img")
-              text " "
-            end
-            text award.issuer_display_name
-          }
-          if project.ethereum_enabled
-            column("small-2", class: 'blockchain-address') {
-              if award.ethereum_transaction_explorer_url
-                link_to award.ethereum_transaction_address_short, award.ethereum_transaction_explorer_url, target: '_blank'
-              elsif award.recipient_address.blank? && current_account == award.recipient_account && show_recipient
-                link_to '(no account)', account_path
-              elsif award.recipient_address.blank?
-                text '(no account)'
-              else
-                text '(pending)'
+
+          awards.sort_by(&:created_at).reverse.each do |award|
+            tr(class: "award-row") {
+              td(class: "small-1") {
+                text number_with_delimiter(award.award_type.amount, :delimiter => ',')
+              }
+              td(class: "small-2") {
+                text raw award.created_at.strftime("%b %d, %Y").gsub(' ', '&nbsp;')
+              }
+              if show_recipient
+                td(class: "small-2") {
+                  img(src: award.authentication.slack_icon, class: "icon avatar-img")
+                  text " " + award.recipient_display_name
+                }
+              end
+              td(class: "small-3") {
+                if award.proof_link
+                  link_to award.proof_id_short, award.proof_link, target: '_blank'
+                else
+                  span award.proof_id_short
+                end
+                br
+                strong "#{award.award_type.name}"
+                text raw ": #{markdown_to_html award.description}" if award.description.present?
+              }
+              td(class: "small-2") {
+                if award.issuer_slack_icon
+                  img(src: award.issuer_slack_icon, class: "icon avatar-img")
+                  text " "
+                end
+                text award.issuer_display_name
+              }
+              if project.ethereum_enabled
+                td(class: "small-2", class: 'blockchain-address') {
+                  if award.ethereum_transaction_explorer_url
+                    link_to award.ethereum_transaction_address_short, award.ethereum_transaction_explorer_url, target: '_blank'
+                  elsif award.recipient_address.blank? && current_account == award.recipient_account && show_recipient
+                    link_to '(no account)', account_path
+                  elsif award.recipient_address.blank?
+                    text '(no account)'
+                  else
+                    text '(pending)'
+                  end
+                }
               end
             }
           end
         }
-      end
+      }
     }
   end
 end
