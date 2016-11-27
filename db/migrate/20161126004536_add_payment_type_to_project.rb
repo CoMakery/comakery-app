8ac174dcca84ca341a78f3e05046f73bc77644ec
Harlan T Wood and Noah Thorp <dev+harlan+noah@comakery.com>
2016-11-27 15:58:50 -0800

    Add down migration and check that down migrations are possible on CI

--- db/migrate/20161126004536_add_payment_type_to_project.rb
+++ db/migrate/20161126004536_add_payment_type_to_project.rb
@@ -1,6 +1,10 @@
 class AddPaymentTypeToProject < ActiveRecord::Migration
-  def change
+  def up
     add_column :projects, :payment_type, :integer, default: 1 # sets all existing project to "project_coin"
     change_column :projects, :payment_type, :integer, default: 0 # future projects default to "royalty_usd"
+  end
+
+  def down
+    remove_column :projects, :payment_type
   end
 end
