29935455041543542795edbb0b62fa5ea8b6bba5
Noah Thorp <dev+noah@comakery.com>
2017-03-26 21:19:03 -0400

    Calculation table view test coverage

---
+++ spec/views/shared/table/_unpaid_pool_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+describe "shared/table/_unpaid_pool.html.rb" do
+  let(:project) { raise "define let(:project) in your describe block" }
+  let(:current_auth) { create :authentication }
+
+  before do
+    assign :project, project.decorate
+    assign :current_auth, current_auth.decorate
+    render
+  end
+
+  describe 'project coin' do
+    let(:project) { create :project, payment_type: :project_coin }
+
+    specify { expect(rendered).to eq("") }
+  end
+
+  describe 'USD' do
+    let(:project) { create :project, denomination: 'USD' }
+
+    specify do
+      expect(rendered).to have_css('.total-revenue-shared', text: '$0.00')
+    end
+
+    specify do
+      expect(rendered).to have_css('.total-paid-to-contributors', text: '$0.00')
+    end
+
+    specify do
+      expect(rendered).to have_css('.revenue-shared-unpaid', text: '$0.00')
+    end
+  end
+
+  describe 'BTC' do
+    let(:project) { create :project, denomination: 'BTC' }
+
+    specify do
+      expect(rendered).to have_css('.total-revenue-shared', text: '฿0.00')
+    end
+
+    specify do
+      expect(rendered).to have_css('.total-paid-to-contributors', text: '฿0.00')
+    end
+
+    specify do
+      expect(rendered).to have_css('.revenue-shared-unpaid', text: '฿0.00')
+    end
+  end
+end
---
+++ spec/views/shared/table/_unpaid_revenue_shares_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+describe "shared/table/_unpaid_revenue_shares.html.rb" do
+  let(:project) { raise "define let(:project) in your describe block" }
+  let(:current_auth) { create :authentication }
+
+  before do
+    assign :project, project.decorate
+    assign :current_auth, current_auth.decorate
+    render
+  end
+
+  describe 'project coin' do
+    let(:project) { create :project, payment_type: :project_coin }
+
+    specify { expect(rendered).to eq("") }
+  end
+
+  describe 'revenue shares' do
+    let(:project) { create :project, denomination: 'USD' }
+
+    specify do
+      expect(rendered).to have_css('.total-awarded', text: '0')
+    end
+
+    specify do
+      expect(rendered).to have_css('.awards-redeemed', text: '0')
+    end
+
+    specify do
+      expect(rendered).to have_css('.awards-outstanding', text: '0')
+    end
+  end
+end
