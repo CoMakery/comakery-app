056fd45160133e061685eb90ae5912020ce942b8
Prasad <bhanurorkitchen2@gmail.com>
2018-03-06 13:10:51 +0700

    test for award type model

--- spec/models/award_type_spec.rb
+++ spec/models/award_type_spec.rb
@@ -23,7 +23,7 @@ describe AwardType do
   end
 
   describe 'associations' do
-    let(:project) { create(:project, owner_account: create(:account)) }
+    let(:project) { create(:project, account: create(:account)) }
     let(:award_type) { create(:award_type, project: project) }
     let(:award) { create(:award, award_type: award_type) }
 
@@ -99,13 +99,9 @@ describe AwardType do
 
   describe '#awards#create_with_quantity' do
     let(:award_type) { create :award_type, amount: 1 }
-    let(:issuer) { create :account }
-    let(:authentication) { create :authentication }
-    let(:award) do
-      award_type.awards.create_with_quantity 1.4,
-        issuer: issuer,
-        authentication: authentication
-    end
+    let(:account) { create :account }
+    let(:award) { award_type.awards.create_with_quantity 1.4, account: account}
+
 
     specify { expect(award.quantity).to eq(1.4) }
 
@@ -116,9 +112,7 @@ describe AwardType do
     specify { expect(award).to be_persisted }
 
     it 'rounds up when >= x.5' do
-      roundup_award = award_type.awards.create_with_quantity(1.5,
-        issuer: issuer,
-        authentication: authentication)
+      roundup_award = award_type.awards.create_with_quantity(1.5, account: account)
       expect(roundup_award.total_amount).to eq(2)
     end
   end
