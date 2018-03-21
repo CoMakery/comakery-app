13d9c8c589cec76fbe09168d996830d3e8d24176
Prasad <bhanurorkitchen2@gmail.com>
2018-03-21 11:00:15 +0700

    modify migration for ci requirement

--- db/migrate/20180312022660_add_team_id_to_authentication_teams.rb
+++ db/migrate/20180312022660_add_team_id_to_authentication_teams.rb
@@ -3,6 +3,10 @@ class AddTeamIdToAuthenticationTeams < ActiveRecord::Migration[5.1]
     add_column :authentication_teams, :account_id, :integer
     add_column :authentication_teams, :team_id, :integer
     remove_column :authentication_teams, :provider_team_id, :string
-    drop_table :account_teams
+    drop_table :account_teams do |t|
+      t.integer :account_id
+      t.integer :team_id
+      t.timestamps
+    end
   end
 end
