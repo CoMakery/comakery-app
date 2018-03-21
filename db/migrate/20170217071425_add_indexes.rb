262bf87d4c446e0b1f46e4205bf2801f14939fe2
Prasad <bhanurorkitchen2@gmail.com>
2018-03-21 11:24:35 +0700

    fixing migration

--- db/migrate/20170217071425_add_indexes.rb
+++ db/migrate/20170217071425_add_indexes.rb
@@ -5,15 +5,11 @@ class AddIndexes < ActiveRecord::Migration[4.2]
     add_index :award_types, :project_id
 
     add_index :authentications, :account_id
-    add_index :authentications, :slack_user_id
 
     add_index :awards, :issuer_id
     add_index :awards, :award_type_id
-    add_index :awards, :authentication_id
 
     add_index :payments, :project_id
-    add_index :payments, :issuer_id
-    add_index :payments, :recipient_id
 
     add_index :revenues, :project_id
     add_index :revenues, :recorded_by_id
