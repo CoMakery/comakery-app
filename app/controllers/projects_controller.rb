fcdcae38fb023878201f6a37ce5daa6821db521e
Prasad <bhanurorkitchen2@gmail.com>
2018-04-24 16:24:42 +0700

    Landing - group projects by mine, team and archived [skip ci]

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,21 +4,21 @@ class ProjectsController < ApplicationController
 
   def landing
     if current_account
-      @private_projects = current_account.private_projects.with_last_activity_at.limit(6).decorate
+      # @private_projects = current_account.private_projects.with_last_activity_at.limit(6).decorate
+      # @archived_projects = current_account.projects.archived.with_last_activity_at.limit(6).decorate
+      # @unlisted_projects = current_account.projects.unlisted.with_last_activity_at.limit(6).decorate
+      # @public_projects = current_account.public_projects.with_last_activity_at.limit(6).decorate
+      @my_projects = current_account.projects.visible.with_last_activity_at.limit(6).decorate
       @archived_projects = current_account.projects.archived.with_last_activity_at.limit(6).decorate
-      @unlisted_projects = current_account.projects.unlisted.with_last_activity_at.limit(6).decorate
-      @public_projects = current_account.public_projects.with_last_activity_at.limit(6).decorate
+      @team_projects = current_account.team_projects.with_last_activity_at.limit(6).decorate
     else
-      @private_projects = []
       @archived_projects = []
-      @unlisted_projects = []
-      @public_projects = Project.public_listed.featured.with_last_activity_at.limit(6).decorate
+      @team_projects = []
+      @my_projects = Project.public_listed.featured.with_last_activity_at.limit(6).decorate
     end
-    @private_project_contributors = TopContributors.call(projects: @private_projects).contributors
-    @public_project_contributors = TopContributors.call(projects: @public_projects).contributors
+    @my_project_contributors = TopContributors.call(projects: @my_projects).contributors
+    @team_project_contributors = TopContributors.call(projects: @team_projects).contributors
     @archived_project_contributors = TopContributors.call(projects: @archived_projects).contributors
-    @unlisted_project_contributors = TopContributors.call(projects: @unlisted_projects).contributors
-    @slack_auth = current_account&.slack_auth
   end
 
   def index
