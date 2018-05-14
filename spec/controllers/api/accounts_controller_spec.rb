b7bc1cadc2b21233ec6dd29364ce99bc62c8fece
Duc Le <ducle.sg@gmail.com>
2018-05-14 10:08:24 +0700

    rspec for 'find_by_public_address' action with public_address param

--- spec/controllers/api/accounts_controller_spec.rb
+++ spec/controllers/api/accounts_controller_spec.rb
@@ -6,11 +6,19 @@ describe Api::AccountsController do
   let(:signature) { '0x1052ebba4e1efeace65423ee33f3d10c045eb4084d45f96a1435f7c6c1e9f5c93600cd6c77796183c02c3bc620c28aa5cf6ee31e05d2e858dc8bc7ed3a59bc0b1b' }
 
   describe '#find_by_public_address' do
+    before { post :create, params: { public_address: public_address, network_id: 1 }, format: :json }
+
     it 'without \'public_address\' param' do
       get :find_by_public_address
       expect(response.content_type).to eq 'application/json'
       parsed_response = JSON.parse response.body
       expect(parsed_response).to eq({})
+    end
+
+    it 'with valid \'public_address\' param' do
+      get :find_by_public_address, params: { public_address: public_address }
+      parsed_response = JSON.parse response.body, symbolize_names: true
+      expect(parsed_response[:public_address]).to eq(public_address)
     end
   end
 
