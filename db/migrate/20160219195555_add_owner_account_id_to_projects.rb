83c8c3bb96d208a9c57036a79ee23976edd71876
Prasad <bhanurorkitchen2@gmail.com>
2018-05-14 10:10:40 +0700

    fix migration for production

--- db/migrate/20160219195555_add_owner_account_id_to_projects.rb
+++ db/migrate/20160219195555_add_owner_account_id_to_projects.rb
@@ -4,7 +4,7 @@ class Project < ActiveRecord::Base; end
 class AddOwnerAccountIdToProjects < ActiveRecord::Migration[4.2]
   def up
     add_column :projects, :owner_account_id, :integer
-    Project.update_all(account_id: Account.first.try(:id))
+    Project.update_all(owner_account_id: Account.first.try(:id))
     change_column :projects, :owner_account_id, :integer, null: false
     add_index :projects, :owner_account_id
   end
