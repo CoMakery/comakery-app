3ff9e372d4393e947a8ccb66ec02673715cc1d47
Prasad <bhanurorkitchen2@gmail.com>
2018-05-17 15:27:36 +0700

    test coverage for award type model

--- spec/models/award_type_spec.rb
+++ spec/models/award_type_spec.rb
@@ -115,4 +115,24 @@ describe AwardType do
       expect(roundup_award.total_amount).to eq(2)
     end
   end
+
+  it 'invalid_params' do
+    attributes = {}
+    expect(described_class.invalid_params(attributes)).to eq true
+    attributes['name'] = 'name'
+    expect(described_class.invalid_params(attributes)).to eq true
+    attributes['name'] = nil
+    attributes['amount'] = 10
+    expect(described_class.invalid_params(attributes)).to eq true
+    attributes['name'] = 'name'
+    attributes['amount'] = 10
+    expect(described_class.invalid_params(attributes)).to eq false
+  end
+
+  it 'check for active status' do
+    award_type = create :award_type
+    expect(award_type.active?).to eq true
+    award_type.update disabled: true
+    expect(award_type.reload.active?).to eq false
+  end
 end
