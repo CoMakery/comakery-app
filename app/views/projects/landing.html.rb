55c3a926aae360b215686dcf964668c5efe1d2d5
Duc Le <ducle.sg@gmail.com>
2018-05-18 09:30:21 +0700

    add a sign in with metamask button to the home page

--- app/views/projects/landing.html.rb
+++ app/views/projects/landing.html.rb
@@ -45,7 +45,9 @@ class Views::Projects::Landing < Views::Projects::Base
             }
             a('Sign in with Slack', class: buttonish << 'margin-small', href: login_path)
             a('Sign in with Discord', class: buttonish << 'margin-small', href: login_discord_path)
+            a('Sign in with MetaMask', class: buttonish << 'margin-small signin-with-metamask', href: 'javascript:void(0)')
             a("or join #{t('company_name')}'s Slack", class: 'beta-signup', href: t('company_public_slack_url'))
+            render 'sessions/metamask_modal'
           }
         }
         div(class: 'how-it-works') {
