69e89bfb602b35bc358217c8f92537847d197b01
Prasad <bhanurorkitchen2@gmail.com>
2018-06-27 09:52:27 +0700

    everyone can see award progress bar

--- app/views/shared/_award_progress_bar.html.rb
+++ app/views/shared/_award_progress_bar.html.rb
@@ -2,7 +2,6 @@ class Views::Shared::AwardProgressBar < Views::Base
   needs :project, :current_account_deco
 
   def content
-    return unless current_account_deco.present? && current_account_deco.same_team_or_owned_project?(project)
     div(class: 'meter-box') {
       div(class: 'meter-text') {
         column('small-6', style: 'padding-left: 0') {
@@ -23,28 +22,6 @@ class Views::Shared::AwardProgressBar < Views::Base
     }
   end
 
-  def meter_text_balances
-    text 'My Balance'
-    br
-    my_share
-    if project.revenue_share?
-      br
-      my_balance
-    end
-  end
-
-  def my_share
-    span(class: ' my-share ') {
-      text "#{project.payment_description} #{current_account_deco.total_awards_remaining_pretty(project)}"
-    }
-  end
-
-  def my_balance
-    span(class: ' my-balance ') {
-      text current_account_deco.total_revenue_unpaid_remaining_pretty(project).to_s
-    }
-  end
-
   def meter(left: ' ', right: ' ')
     div(class: ' meter ') {
       span(class: ' complete ', style: "width: #{project.percent_awarded_pretty};") {
@@ -52,17 +29,5 @@ class Views::Shared::AwardProgressBar < Views::Base
       }
       span(class: ' incomplete ') { text right }
     }
-  end
-
-  def percent_mine_pretty
-    "#{current_account_deco.percentage_of_unpaid_pretty(project)} Mine"
-  end
-
-  def complete
-    div(class: ' complete-text ', style: "width: #{current_account_deco.percentage_of_unpaid_pretty(project)}") { yield }
-  end
-
-  def incomplete
-    div(class: ' incomplete-text ') { yield }
   end
 end
