ef12e52e5fdfa38f7893559b422f6f95d1f25a85
Duc Le <ducle.sg@gmail.com>
2018-06-27 11:20:34 +0700

    add spec for 'json_for_sending_awards' in award decorator

--- spec/decorators/award_decorator_spec.rb
+++ spec/decorators/award_decorator_spec.rb
@@ -43,6 +43,23 @@ describe AwardDecorator do
     end
   end
 
+  context 'json_for_sending_awards' do
+    let!(:recipient) { create :account, id: 529, ethereum_wallet: '0xD8655aFe58B540D8372faaFe48441AeEc3bec488' }
+    let!(:issuer) { create :account, first_name: 'johnny', last_name: 'johnny', ethereum_wallet: '0xD8655aFe58B540D8372faaFe48441AeEc3bec453' }
+    let!(:project) { create :project, id: 512, account: issuer, ethereum_contract_address: '0x8023214bf21b1467be550d9b889eca672355c005' }
+    let!(:award_type) { create :award_type, project: project }
+    let!(:award) { create :award, id: 521, award_type: award_type, issuer: issuer, account: recipient }
+
+    it 'valid' do
+      expected = %({"id":521,"total_amount":1337,"issuer_address":"0xD8655aFe58B540D8372faaFe48441AeEc3bec453","account":{"id":529,"ethereum_wallet":"0xD8655aFe58B540D8372faaFe48441AeEc3bec488"},"project":{"id":512,"ethereum_contract_address":"0x8023214bf21b1467be550d9b889eca672355c005"}})
+      expect(award.decorate.json_for_sending_awards).to eq(expected)
+    end
+
+    it 'invalid' do
+      expect(award.decorate.json_for_sending_awards).not_to eq(award.decorate.to_json(only: %i[id total_amount], methods: [:issuer_address]))
+    end
+  end
+
   it 'return ethereum_transaction_address_short' do
     award = create :award, ethereum_transaction_address: '0xb808727d7968303cdd6486d5f0bdf7c0f690f59c1311458d63bc6a35adcacedb'
     expect(award.decorate.ethereum_transaction_address_short).to eq '0xb808727d...'
