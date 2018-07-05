31bcd06c955322281e766cfed94d43f4efc93d23
Prasad <bhanurorkitchen2@gmail.com>
2018-07-05 17:07:09 +0700

    landing page - header

--- app/views/pages/landing.html.rb
+++ app/views/pages/landing.html.rb
@@ -1,3 +1,17 @@
 class Views::Pages::Landing < Views::Base
-  def content; end
+  def content
+    content_for :pre_body do
+      div(class: 'landing-header'){
+        h1{
+          text 'JOIN INCREDIABLE BLOCKCHAIN PROJECTS'
+        }
+        h3{
+          text 'DISCOVER PROJECTS | LIST YOUR OWN | GET PAID IN TOKENS'
+        }
+      }
+      div(class: 'whois'){
+        image_tag 'whois/H1-TEXT.png'
+      }
+    end
+  end
 end
