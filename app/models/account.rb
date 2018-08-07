217bb86f2f0036c9795b6a965fdf6e414b8b7bd7
Prasad <bhanurorkitchen2@gmail.com>
2018-08-07 10:02:02 +0700

    fix join awards duplicate

--- app/models/account.rb
+++ app/models/account.rb
@@ -123,21 +123,21 @@ class Account < ApplicationRecord
   def other_member_projects
     Project.joins("
       left join award_types at1 on at1.project_id=projects.id
-      left join awards on awards.award_type_id=at1.id
+      left join awards a1 on a1.award_type_id=at1.id
       left join channels on channels.project_id=projects.id
       left join teams on teams.id=channels.team_id
       left join authentication_teams on authentication_teams.team_id=teams.id")
-           .where("((authentication_teams.account_id=#{id} and channels.id is not null) or awards.account_id=#{id}) and projects.account_id <> #{id}").distinct
+           .where("((authentication_teams.account_id=#{id} and channels.id is not null) or a1.account_id=#{id}) and projects.account_id <> #{id}").distinct
   end
 
   def accessable_projects
     Project.joins("
       left join award_types at1 on at1.project_id=projects.id
-      left join awards on awards.award_type_id=at1.id
+      left join awards a1 on a1.award_type_id=at1.id
       left join channels on channels.project_id=projects.id
       left join teams on teams.id=channels.team_id
       left join authentication_teams on authentication_teams.team_id=teams.id")
-           .where("(authentication_teams.account_id=#{id} and channels.id is not null) or projects.visibility=1 or awards.account_id=#{id} or projects.account_id=#{id}").distinct
+           .where("(authentication_teams.account_id=#{id} and channels.id is not null) or projects.visibility=1 or a1.account_id=#{id} or projects.account_id=#{id}").distinct
   end
 
   def confirmed?
