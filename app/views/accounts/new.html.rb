0e86bdee645b5996883788c6ab7ada7f70483505
Prasad <bhanurorkitchen2@gmail.com>
2018-09-06 08:42:07 +0700

    update signup page - save user agreement date with social accounts

--- app/views/accounts/new.html.rb
+++ app/views/accounts/new.html.rb
@@ -77,12 +77,13 @@ class Views::Accounts::New < Views::Base
                 label do
                   f.check_box :agreed_to_user_agreement
                   span do
-                    text 'I agree to the terms of the '
+                    text 'I agree to the '
                     link_to 'CoMakery User Agreement', '/user-agreement'
+                    text ' and '
+                    link_to 'Privacy Policy terms', '/privacy-policy'
                   end
                 end
               end
-
               f.submit 'CREATE YOUR ACCOUNT', class: buttonish(:medium), style: 'margin: 0'
             end
           end
@@ -103,6 +104,14 @@ class Views::Accounts::New < Views::Base
         column('large-12 no-h-pad', style: 'margin-top: 10px') do
           link_to login_discord_path, class: 'auth-button discord' do
             text 'Discord'
+          end
+        end
+        column('large-12 no-h-pad', style: 'margin-top: 10px') do
+          label do
+            text 'By clicking a button to Sign Up with Slack, Metamask or Discord, you are agreeing to the '
+            link_to 'CoMakery User Agreement', '/user-agreement'
+            text ' and '
+            link_to 'Privacy Policy Terms', '/privacy-policy'
           end
         end
       end
