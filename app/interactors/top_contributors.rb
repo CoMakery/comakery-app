7549b3e361e7bd4997e9db316df9a7b6456c673a
Prasad <bhanurorkitchen2@gmail.com>
2018-10-04 16:11:44 +0700

    fix projects top contributors

--- app/interactors/top_contributors.rb
+++ app/interactors/top_contributors.rb
@@ -3,26 +3,9 @@ class TopContributors
 
   def call
     projects = context.projects
-    n = context.n || 5
-
     contributors = projects.each_with_object({}) do |project, projects_to_contributors|
-      projects_to_contributors[project] = top_contributors(project, n)
+      projects_to_contributors[project] = project.top_contributors
     end
-
     context.contributors = contributors
-  end
-
-  def top_contributors(project, n)
-    all_columns = (Account.column_names - %w[password_digest public_address nonce network_id system_email]).map { |column| "max(accounts.#{column}) as #{column}" }.join(', ')
-    Account
-      .select("#{all_columns}, sum(award_total_amount) as total_awarded, max(last_awarded_at) as last_awarded_at")
-      .from(project.contributors
-            .select("#{all_columns}, sum(awards.total_amount) as award_total_amount, max(awards.created_at) as last_awarded_at")
-            .group('accounts.id, award_types.id'),
-        'accounts')
-      .group('accounts.id')
-      .order('total_awarded desc, max(last_awarded_at) desc')
-      .limit(n)
-      .to_a
   end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -86,13 +86,13 @@ class Project < ApplicationRecord
   end
 
   def top_contributors
-    Account.select('accounts.*, sum(a1.total_amount) as total').joins("
+    Account.select('accounts.*, sum(a1.total_amount) as total_awarded').joins("
       left join awards a1 on a1.account_id=accounts.id
       left join award_types on a1.award_type_id=award_types.id
       left join projects on award_types.project_id=projects.id")
            .where("projects.id=#{id}")
            .group('accounts.id')
-           .order('total desc').first(5)
+           .order('total_awarded desc').first(5)
   end
 
   def create_ethereum_awards!
