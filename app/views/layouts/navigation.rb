4d3fa8e59d93186a05cba23b1f956fd7e0854786
Noah Thorp <noah@comakery.com>
2018-10-14 19:31:50 -0700

    Nav changes: user & avatar links to account, add "MY PROJECTS" link.

--- app/views/layouts/navigation.rb
+++ app/views/layouts/navigation.rb
@@ -1,59 +1,39 @@
 class Views::Layouts::Navigation < Views::Base
   def content
     ul(class: 'menu show-for-medium') do
-      li(class: 'slack-instance') do
-        if current_account
-          div(class: 'top-bar-text') do
-            img(src: account_image_url(current_account, 34), class: 'avatar')
-            text current_account.decorate.name
-          end
-        end
-      end
-      if current_account
-        li do
-          link_to 'ACCOUNT', account_path
-        end
-        li do
-          link_to 'SIGN OUT', session_path, method: :delete
-        end
-      else
-        li do
-          link_to 'CONTACT US', 'mailto:support@comakery.com'
-        end
-        li do
-          link_to 'SIGN IN', new_session_path
-        end
-        li(style: 'padding-right: 0') do
-          link_to 'SIGN UP', new_account_path, class: 'pink-text'
-        end
-      end
+      nav_links
     end
 
     ul(class: 'menu hide-for-medium', style: 'font-size: 14px;') do
-      li(class: 'slack-instance') do
-        if current_account
-          div(class: 'top-bar-text') do
-            text current_account.decorate.name
+      nav_links
+    end
+  end
+
+  def nav_links
+    if current_account
+      li do
+        div(class: 'top-bar-text') do
+          link_to(account_path) do
+            img(src: account_image_url(current_account, 34), class: 'avatar')
           end
+          link_to current_account.decorate.name, account_path
         end
       end
-      if current_account
-        li do
-          link_to 'ACCOUNT', account_path
-        end
-        li do
-          link_to 'SIGN OUT', session_path, method: :delete
-        end
-      else
-        li do
-          link_to 'CONTACT US', 'mailto:support@comakery.com'
-        end
-        li do
-          link_to 'SIGN IN', new_session_path
-        end
-        li do
-          link_to 'SIGN UP', new_account_path, class: 'pink-text'
-        end
+      li do
+        link_to 'MY PROJECTS', my_project_path
+      end
+      li(style: 'padding-right: 0') do
+        link_to 'SIGN OUT', session_path, method: :delete
+      end
+    else
+      li do
+        link_to 'CONTACT US', 'mailto:support@comakery.com'
+      end
+      li do
+        link_to 'SIGN IN', new_session_path
+      end
+      li(style: 'padding-right: 0') do
+        link_to 'SIGN UP', new_account_path, class: 'pink-text'
       end
     end
   end
