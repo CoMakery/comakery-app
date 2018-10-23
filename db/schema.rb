1b4128428cced64138a8a41a97c7ba918729941a
Prasad <bhanurorkitchen2@gmail.com>
2018-10-24 01:09:23 +0700

    interest model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181016093729) do
+ActiveRecord::Schema.define(version: 20181023180146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -132,6 +132,15 @@ ActiveRecord::Schema.define(version: 20181016093729) do
     t.index ["team_id"], name: "index_channels_on_team_id"
   end
 
+  create_table "interests", force: :cascade do |t|
+    t.bigint "account_id"
+    t.string "protocol"
+    t.string "project"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["account_id"], name: "index_interests_on_account_id"
+  end
+
   create_table "payments", id: :serial, force: :cascade do |t|
     t.integer "project_id"
     t.integer "issuer_id"
@@ -212,4 +221,5 @@ ActiveRecord::Schema.define(version: 20181016093729) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "interests", "accounts"
 end
