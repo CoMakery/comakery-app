dff35a78017888a10a8f09a153c56cb55059f81c
Prasad <bhanurorkitchen2@gmail.com>
2018-10-29 06:20:23 +0700

    Implement Airtable Service

---
+++ app/lib/comakery/airtable.rb
@@ -0,0 +1,32 @@
+require 'rest-client'
+class Comakery::Airtable
+  def initialize
+    @token = "Bearer #{ENV['AIRTABLE_API']}"
+    @url = 'https://api.airtable.com/v0/appnG7FbWtdxxDGXd/Table%204'
+  end
+
+  def list
+    @url += "?maxRecords=10&view=Grid%20view"
+    result
+  end
+
+  def add_record(params)
+    @data = { fields: params }.to_json
+    post_result
+  end
+
+  private
+
+  def result
+    res = RestClient.get(@url, Authorization: @token)
+    JSON.parse(res)
+  rescue RestClient::Forbidden => e
+    { error: e.message }
+  end
+
+  def post_result(parse_json = true)
+    header = { Authorization: @token, content_type: :json, accept: :json }
+    res = RestClient.post(@url, @data, header)
+    parse_json ? JSON.parse(res) : res
+  end
+end
--- app/models/interest.rb
+++ app/models/interest.rb
@@ -1,3 +1,10 @@
 class Interest < ApplicationRecord
   belongs_to :account
+  after_save :add_airtable
+
+  def add_airtable
+    params = {"ID": id, "Acount ID": account_id, "Protocol Interest": protocol, "Project Interest", "Email Address": account.email, "First Name": account.first_name, "Last Name": account.last_name}
+    airtable = Comakery::Airtable.new
+    airtable.add_record(params)
+  end
 end
