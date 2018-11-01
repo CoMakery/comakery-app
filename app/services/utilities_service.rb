2d5c74b9d3676cdb685c26f76f9c1f4a36823f9c
Duc Le <ducle.sg@gmail.com>
2018-11-01 16:08:47 +0700

    fix wallet url is incorrect

--- app/services/utilities_service.rb
+++ app/services/utilities_service.rb
@@ -1,5 +1,9 @@
 class UtilitiesService
   def self.get_wallet_url(network, wallet)
-    "https://#{network}.etherscan.io/address/#{wallet}"
+    if network.blank? || network == 'main'
+      "https://etherscan.io/address/#{wallet}"
+    else
+      "https://#{network}.etherscan.io/address/#{wallet}"
+    end
   end
 end
