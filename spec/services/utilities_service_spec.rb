866c89a10c1b6db4d9f1859f0b1a2303cc1d3b02
Duc Le <ducle.sg@gmail.com>
2018-11-07 19:30:55 +0700

    add spec for url of wallet on qtum network

--- spec/services/utilities_service_spec.rb
+++ spec/services/utilities_service_spec.rb
@@ -4,6 +4,14 @@ describe UtilitiesService do
   let(:wallet) { '0xaBe4449277c893B3e881c29B17FC737ff527Fa47' }
 
   context '.get_wallet_url' do
+    it 'without network' do
+      network = nil
+
+      result = described_class.get_wallet_url(network, wallet)
+
+      expect(result).to eq 'javascript:void(0);'
+    end
+
     it 'wallet on Mainnet' do
       network = 'main'
 
@@ -34,6 +42,22 @@ describe UtilitiesService do
       result = described_class.get_wallet_url(network, wallet)
 
       expect(result).to eq "https://rinkeby.etherscan.io/address/#{wallet}"
+    end
+
+    it 'wallet on qtum mainnet' do
+      network = 'qtum_mainnet'
+
+      result = described_class.get_wallet_url(network, wallet)
+
+      expect(result).to eq "https://explorer.qtum.org/address/#{wallet}"
+    end
+
+    it 'wallet on qtum testnet' do
+      network = 'qtum_testnet'
+
+      result = described_class.get_wallet_url(network, wallet)
+
+      expect(result).to eq "https://testnet.qtum.org/address/#{wallet}"
     end
   end
 end
