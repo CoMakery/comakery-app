ec93574ea4b596b594b0c442ee5a7776832d6ff3
Jashan Mudhar <jash.mud@outlook.com>
2018-11-08 03:08:53 +1000

    EditState after saving

--- app/javascript/components/Account.jsx
+++ app/javascript/components/Account.jsx
@@ -61,6 +61,7 @@ export default class Account extends React.Component {
           message    : response.message,
           messageType: 'notice',
           showMessage: true,
+          isEdit     : false,
           accountData: response.current_acccount,
         })
       },
@@ -129,184 +130,115 @@ export default class Account extends React.Component {
       </div>
     )
 
-    return (
-      <React.Fragment>
-        <Alert
-          message={this.state.message}
-          messageType={this.state.messageType}
-          isVisible={this.state.showMessage}
-          toggleVisible={() => {
-            this.setState({ showMessage: !this.state.showMessage })
-          }}
-        />
-        <div className="ethereum_wallet m-t-10">
-          <div className={`${this.state.isEdit ? '' : 'hide'} edit-ethereum-wallet`}>
-            <h4 style={{ border: 'none' }}>
-              Account Detail (
-              <a href="#" onClick={this.handleChangeEditMode}>
-                Cancel
-              </a>
-              )
-            </h4>
-            <div className="row">
-              <form onSubmit={this.handleUpdateAccountInfo}>
-                <FormField
-                  fieldLabel="Email"
-                  fieldName="email"
-                  fieldValue={this.state.email}
-                  handleChange={this.handleChangeAccountFormData}
-                  error={this.state.errors.email}
-                />
-                <FormField
-                  fieldLabel="First Name"
-                  fieldName="first_name"
-                  fieldValue={this.state.first_name}
-                  handleChange={this.handleChangeAccountFormData}
-                  error={this.state.errors.first_name}
-                />
-                <FormField
-                  fieldLabel="Last Name"
-                  fieldName="last_name"
-                  fieldValue={this.state.last_name}
-                  handleChange={this.handleChangeAccountFormData}
-                  error={this.state.errors.last_name}
-                />
-                <FormField
-                  fieldLabel="Nickname"
-                  fieldName="nickname"
-                  fieldValue={this.state.nickname}
-                  handleChange={this.handleChangeAccountFormData}
-                  error={this.state.errors.nickname}
-                />
-                <div className="columns small-3">
-                  <label>Date of Birth</label>
-                </div>
-                <div className={`columns small-9 ${this.state.errors.date_of_birth ? 'error' : ''}`}>
-                  <input
-                    type="text"
-                    className="datepicker"
-                    placeholder="mm/dd/yyyy"
-                    name="date_of_birth"
-                    defaultValue={this.state.date_of_birth || ''}
-                    ref={this.dateInput}
-                  />
-                  {this.state.errors.date_of_birth &&
-                  <small className="error">
-                    {this.state.errors.date_of_birth}
-                  </small>}
-                </div>
-                <div className="columns small-3">
-                  <label>Country</label>
-                </div>
-                <div className={`columns small-9 ${this.state.errors.country ? 'error' : ''}`}>
-                  <select
-                    name="country"
-                    value={this.state.country || ''}
-                    onChange={this.handleChangeAccountFormData}
-                  >
-                    <option value="">Select Country</option>
-                    {this.props.countryList.map(country =>
-                      <option key={country.data.name} value={country.data.name}>
-                        {country.data.name}
-                      </option>
-                    )}
-                  </select>
-                  {this.state.errors.country &&
-                  <small className="error">
-                    {this.state.errors.country}
-                  </small>}
-                </div>
-                <FormField
-                  fieldLabel="Ethereum Address"
-                  fieldName="ethereum_wallet"
-                  fieldValue={this.state.ethereum_wallet}
-                  handleChange={this.handleChangeAccountFormData}
-                  error={this.state.errors.ethereum_wallet}
-                />
-                <div className="columns small-3">
-                  <label>Image</label>
-                </div>
-                <div className="columns small-9">
-                  <input type="file" name="image" ref={this.fileInput} />
-                </div>
-                <div className="columns small-12 text-right">
-                  <input type="submit" value="Save" className="button" />
-                </div>
-              </form>
-            </div>
+    return <React.Fragment>
+      <Alert message={this.state.message} messageType={this.state.messageType} isVisible={this.state.showMessage} toggleVisible={() => {
+        this.setState({ showMessage: !this.state.showMessage })
+      }} />
+      <div className="ethereum_wallet m-t-10">
+        <div className={`${this.state.isEdit ? '' : 'hide'} edit-ethereum-wallet`}>
+          <h4 style={{ border: 'none' }}>
+            Account Detail (
+            <a href="#" onClick={this.handleChangeEditMode}>
+              Cancel
+            </a>
+            )
+          </h4>
+          <div className="row">
+            <form onSubmit={this.handleUpdateAccountInfo}>
+              <FormField fieldLabel="Email" fieldName="email" fieldValue={this.state.email} handleChange={this.handleChangeAccountFormData} error={this.state.errors.email} />
+              <FormField fieldLabel="First Name" fieldName="first_name" fieldValue={this.state.first_name} handleChange={this.handleChangeAccountFormData} error={this.state.errors.first_name} />
+              <FormField fieldLabel="Last Name" fieldName="last_name" fieldValue={this.state.last_name} handleChange={this.handleChangeAccountFormData} error={this.state.errors.last_name} />
+              <FormField fieldLabel="Nickname" fieldName="nickname" fieldValue={this.state.nickname} handleChange={this.handleChangeAccountFormData} error={this.state.errors.nickname} />
+              <div className="columns small-3">
+                <label>Date of Birth</label>
+              </div>
+              <div className={`columns small-9 ${this.state.errors.date_of_birth ? 'error' : ''}`}>
+                <input type="text" className="datepicker" placeholder="mm/dd/yyyy" name="date_of_birth" defaultValue={this.state.date_of_birth || ''} ref={this.dateInput} />
+                {this.state.errors.date_of_birth && <small className="error">
+                  {this.state.errors.date_of_birth}
+                </small>}
+              </div>
+              <div className="columns small-3">
+                <label>Country</label>
+              </div>
+              <div className={`columns small-9 ${this.state.errors.country ? 'error' : ''}`}>
+                <select name="country" value={this.state.country || ''} onChange={this.handleChangeAccountFormData}>
+                  <option value="">Select Country</option>
+                  {this.props.countryList.map(country =>
+                    <option key={country.data.name} value={country.data.name}>
+                      {country.data.name}
+                    </option>
+                  )}
+                </select>
+                {this.state.errors.country && <small className="error">
+                  {this.state.errors.country}
+                </small>}
+              </div>
+              <FormField fieldLabel="Ethereum Address" fieldName="ethereum_wallet" fieldValue={this.state.ethereum_wallet} handleChange={this.handleChangeAccountFormData} error={this.state.errors.ethereum_wallet} />
+              <div className="columns small-3">
+                <label>Image</label>
+              </div>
+              <div className="columns small-9">
+                <input type="file" name="image" ref={this.fileInput} />
+              </div>
+              <div className="columns small-12 text-right">
+                <input type="submit" value="Save" className="button" />
+              </div>
+            </form>
           </div>
-          <div className={`${this.state.isEdit ? 'hide' : ''} view-ethereum-wallet`}>
-            <div className="columns medium-12 large-9 no-h-pad">
-              <h4 style={{ border: 'none' }}>
-                Account Details&nbsp;
-                <a href="#" onClick={this.handleChangeEditMode}>
-                  <i className="fa fa-cog" />
-                </a>
-              </h4>
-              <DataField fieldName="Email" fieldValue={this.state.accountData.email} />
-              <DataField fieldName="First Name" fieldValue={this.state.accountData.first_name} />
-              <DataField fieldName="Last Name" fieldValue={this.state.accountData.last_name} />
-              <DataField fieldName="Nickname" fieldValue={this.state.accountData.nickname} />
-              <DataField fieldName="Date of Birth" fieldValue={this.state.accountData.date_of_birth} />
-              <DataField fieldName="Country" fieldValue={this.state.accountData.country} />
-              <div className="row">
-                <div className="columns medium-3" style={{ marginTop: 8 }}>
-                  Ethereum Address
-                </div>
-                <div className="columns medium-9">
-                  {this.state.accountData.ethereum_wallet &&
-                  <React.Fragment>
-                    <input
-                      type="text"
-                      value={this.state.accountData.ethereum_wallet}
-                      readOnly
-                      className="fake-link copy-source fake-link--input"
-                      data-href={this.state.accountData.etherscan_address}
-                    />
-                    <a className="copiable copiable--link">
-                      <img src={this.state.accountData.clippy_icon} width={20} height={20} />
-                    </a>
-                  </React.Fragment>}
-                </div>
+        </div>
+        <div className={`${this.state.isEdit ? 'hide' : ''} view-ethereum-wallet`}>
+          <div className="columns medium-12 large-9 no-h-pad">
+            <h4 style={{ border: 'none' }}>
+              Account Details&nbsp;
+              <a href="#" onClick={this.handleChangeEditMode}>
+                <i className="fa fa-cog" />
+              </a>
+            </h4>
+            <DataField fieldName="Email" fieldValue={this.state.accountData.email} />
+            <DataField fieldName="First Name" fieldValue={this.state.accountData.first_name} />
+            <DataField fieldName="Last Name" fieldValue={this.state.accountData.last_name} />
+            <DataField fieldName="Nickname" fieldValue={this.state.accountData.nickname} />
+            <DataField fieldName="Date of Birth" fieldValue={this.state.accountData.date_of_birth} />
+            <DataField fieldName="Country" fieldValue={this.state.accountData.country} />
+            <div className="row">
+              <div className="columns medium-3" style={{ marginTop: 8 }}>
+                Ethereum Address
+              </div>
+              <div className="columns medium-9">
+                {this.state.accountData.ethereum_wallet && <React.Fragment>
+                  <input type="text" value={this.state.accountData.ethereum_wallet} readOnly className="fake-link copy-source fake-link--input" data-href={this.state.accountData.etherscan_address} />
+                  <a className="copiable copiable--link">
+                    <img src={this.props.clippyIcon} width={20} height={20} />
+                  </a>
+                </React.Fragment>}
               </div>
             </div>
-            <div className="columns show-for-large medium-12 large-3 text-right">
-              {downloadWidget}
-            </div>
-            <div className="columns hide-for-large medium-12" style={{ marginBottom: 20 }}>
-              {downloadWidget}
-            </div>
+          </div>
+          <div className="columns show-for-large medium-12 large-3 text-right">
+            {downloadWidget}
+          </div>
+          <div className="columns hide-for-large medium-12" style={{ marginBottom: 20 }}>
+            {downloadWidget}
           </div>
         </div>
-        <div className="columns medium-12 no-h-pad">
-          <div style={{ float: 'left', width: 90 }}>
-            <label>
-              <input type="radio" checked={this.state.isSummary} onChange={this.handleChangeTableView} />
-              Summary
-            </label>
-          </div>
-          <div style={{ float: 'left', width: 90 }}>
-            <label>
-              <input type="radio" checked={!this.state.isSummary} onChange={this.handleChangeTableView} />
-              History
-            </label>
-          </div>
+      </div>
+      <div className="columns medium-12 no-h-pad">
+        <div style={{ float: 'left', width: 90 }}>
+          <label>
+            <input type="radio" checked={this.state.isSummary} onChange={this.handleChangeTableView} />
+            Summary
+          </label>
         </div>
-        {this.state.isSummary
-          ? <SummaryTable
-            projects={this.state.projects}
-            currentPage={this.state.projectPage}
-            pageCount={Math.ceil(this.props.projectsCount / 20)}
-            handlePageChange={this.handleChangeProjectPage}
-          />
-          : <HistoryTable
-            awards={this.state.awards}
-            currentPage={this.state.awardPage}
-            pageCount={Math.ceil(this.props.awardsCount / 20)}
-            handlePageChange={this.handleChangeAwardPage}
-          />}
-      </React.Fragment>
-    )
+        <div style={{ float: 'left', width: 90 }}>
+          <label>
+            <input type="radio" checked={!this.state.isSummary} onChange={this.handleChangeTableView} />
+            History
+          </label>
+        </div>
+      </div>
+      {this.state.isSummary ? <SummaryTable projects={this.state.projects} currentPage={this.state.projectPage} pageCount={Math.ceil(this.props.projectsCount / 20)} handlePageChange={this.handleChangeProjectPage} /> : <HistoryTable awards={this.state.awards} currentPage={this.state.awardPage} pageCount={Math.ceil(this.props.awardsCount / 20)} handlePageChange={this.handleChangeAwardPage} />}
+    </React.Fragment>
   }
 }
 
@@ -317,4 +249,5 @@ Account.propTypes = {
   projects      : PropTypes.arrayOf(PropTypes.shape({})).isRequired,
   projectsCount : PropTypes.number.isRequired,
   countryList   : PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+  clippyIcon    : PropTypes.string.isRequired
 }
