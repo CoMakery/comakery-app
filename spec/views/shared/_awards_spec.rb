628584a624b8c472654fb4db650822b0770930eb
Duc Le <ducle.sg@gmail.com>
2018-11-16 21:00:01 +0700

    add spec: show the Qrypto icon on the 'Send' button in awards page

--- spec/views/shared/_awards_spec.rb
+++ spec/views/shared/_awards_spec.rb
@@ -87,14 +87,47 @@ describe 'shared/_awards.html.rb' do
       describe 'with no award ethereum transaction address' do
         describe 'when issuer could send award' do
           before do
-            recipient1.ethereum_wallet = '0x123'
-            project.ethereum_contract_address = '0x' + 'a' * 40
-            project.coin_type = 'erc20'
             assign :current_account, issuer
           end
-          it 'display Metamask icon on Send button' do
-            render
-            expect(rendered).to have_css 'img[alt=Metamask2]'
+          context 'coin_type eq erc20' do
+            before do
+              recipient1.ethereum_wallet = '0x123'
+              project.ethereum_contract_address = '0x' + 'a' * 40
+              project.coin_type = 'erc20'
+            end
+            it 'display Metamask icon on Send button' do
+              render
+              expect(rendered).to have_css 'img[alt=Metamask2]'
+            end
+          end
+
+          context 'coin_type eq eth' do
+            before do
+              recipient1.ethereum_wallet = '0x123'
+              project.coin_type = 'eth'
+            end
+            it 'display Metamask icon on Send button' do
+              render
+              expect(rendered).to have_css 'img[alt=Metamask2]'
+            end
+          end
+
+          context 'coin_type eq qrc20' do
+            let!(:project2) do
+              create(:project, account: issuer, ethereum_enabled: true, contract_address: 'a' * 40, coin_type: 'qrc20')
+            end
+            let!(:award_type2) { create(:award_type, project: project2) }
+            let!(:award2) { create(:award, award_type: award_type2, description: 'markdown _rocks_: www.auto.link', issuer: issuer, account: recipient1).decorate }
+
+            before do
+              recipient1.qtum_wallet = 'q123'
+              assign :project, project2.decorate
+              assign :awards, [award2]
+            end
+            it 'display Qrypto icon on Send button' do
+              render
+              expect(rendered).to have_css 'img[alt=Qrypto]'
+            end
           end
         end
 
