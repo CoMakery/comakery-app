1a0128ab519c4d7e621f8e916a8cefdde4d4b198
Jashan Mudhar <jash.mud@outlook.com>
2018-11-27 03:23:24 +1000

    Qtum wallet address

--- app/views/accounts/show.html.rb
+++ app/views/accounts/show.html.rb
@@ -5,6 +5,7 @@ class Views::Accounts::Show < Views::Base
     text react_component 'Account',
       currentAccount: current_account.as_json(only: %i[email first_name last_name nickname date_of_birth country qtum_wallet ethereum_wallet]).merge(
         etherscan_address: current_account.decorate.etherscan_address,
+        qtum_address: current_account.decorate.qtum_wallet_url,
         image_url: current_account.image.present? ? attachment_url(current_account, :image, :fill, 190, 190) : nil
       ),
       awards: awards,
