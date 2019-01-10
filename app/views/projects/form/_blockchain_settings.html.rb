8be6a6d4eb06b09d5bbacf63dccf3ec3f0f1140d
Duc Le <ducle.sg@gmail.com>
2019-01-10 09:35:29 +0700

    move customBlockchainNetwork function into app/assets/javascripts

--- app/views/projects/form/_blockchain_settings.html.rb
+++ app/views/projects/form/_blockchain_settings.html.rb
@@ -215,27 +215,6 @@ class Views::Projects::Form::BlockchainSettings < Views::Base
 
   def coin_type_on_change
     text(<<-JAVASCRIPT.html_safe)
-      function customBlockchainNetwork(coinType) {
-        var prefix = ''
-        switch(coinType) {
-        case 'qrc20':
-          prefix = 'qtum_'
-          break;
-        case 'ada':
-          prefix = 'cardano_'
-          break;
-        }
-        if(prefix != '') {
-          $("[name='project[blockchain_network]']").children('option:not(:first)').remove()
-          var data = JSON.parse($("[name='project[blockchain_network]']").attr('data-info'))
-          $.each(data, (k,v) => {
-            if(k.startsWith(prefix)) {
-              $("[name='project[blockchain_network]']").append(new Option(v, k))
-            }
-          })
-        }
-      }
-
       $(function() {
         $('body').on('change', "[name='project[coin_type]']", function() {
           var coinType = $('#project_coin_type option:selected').val()
