4357a9334793b9149ed9948d75615bde0c1200f5
Duc Le <ducle.sg@gmail.com>
2019-01-18 11:28:51 +0700

    add spec for bitcoin selectTxs function

--- spec/javascript/networks/bitcoin/helpers/utils.spec.js
+++ spec/javascript/networks/bitcoin/helpers/utils.spec.js
@@ -3,7 +3,7 @@ import bitcoinUtils from 'networks/bitcoin/helpers/utils'
 
 jest.mock('axios')
 
-describe('bitcoin utils', () => {
+describe('bitcoin utils #getFee', () => {
   afterEach(() => {
     jest.resetAllMocks()
   })
@@ -16,5 +16,31 @@ describe('bitcoin utils', () => {
     expect(rs).toEqual(0.0000765)
     expect(axios.get).toHaveBeenCalledTimes(1)
     expect(axios.get).toHaveBeenCalledWith('https://bitcoinfees.earn.com/api/v1/fees/recommended')
+  })
+
+  it('get fee with error from axios', async() => {
+    axios.get.mockImplementation(() => Promise.reject({error: true}))
+    const rs = await bitcoinUtils.getFee()
+    expect(rs).toEqual(0.0001)
+    expect(axios.get).toHaveBeenCalledTimes(1)
+    expect(axios.get).toHaveBeenCalledWith('https://bitcoinfees.earn.com/api/v1/fees/recommended')
+  })
+})
+
+describe('bitcoin utils #selectTxs', () => {
+  const fixtures = require('./fixtures/utils-selectTxs.json')
+  const errorsFixtures = require('./fixtures/utils-selectTxs-errors.json')
+  fixtures.forEach(({description, unspentTransactions, amount, fee, result}) => {
+    it(description, async() => {
+      const rs = await bitcoinUtils.selectTxs(unspentTransactions, amount, fee)
+      expect(rs).toEqual(result)
+    })
+  })
+  errorsFixtures.forEach(({description, unspentTransactions, amount, fee, error: errorMessage}) => {
+    it(description, () => {
+      expect(() => {
+        bitcoinUtils.selectTxs(unspentTransactions, amount, fee)
+      }).toThrowError(errorMessage)
+    })
   })
 })
