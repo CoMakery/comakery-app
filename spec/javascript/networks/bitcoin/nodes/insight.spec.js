793952459b3748156d621d6ef1f4b6c6eb50f0db
Duc Le <ducle.sg@gmail.com>
2019-01-21 14:34:28 +0700

    add spec for the getUtxoList function

--- spec/javascript/networks/bitcoin/nodes/insight.spec.js
+++ spec/javascript/networks/bitcoin/nodes/insight.spec.js
@@ -32,3 +32,51 @@ describe('bitcoin insight #getInfo', () => {
     })
   })
 })
+
+describe('bitcoin insight #getUtxoList', () => {
+  const utxo = {
+    'address'      : 'msb16hf6ssyYkAJ8xqKUjmBEkbW3cWCdps',
+    'txid'         : '1e49b7de8dd07eab030f53348641bd85f3df9f74d93231c62f36231ce2b0b7de',
+    'confirmations': 3,
+    'amount'       : 2,
+    'satoshis'     : 200000000,
+    'hash'         : '1e49b7de8dd07eab030f53348641bd85f3df9f74d93231c62f36231ce2b0b7de',
+    'vout'         : 0
+  }
+  const result = {
+    'address'      : 'msb16hf6ssyYkAJ8xqKUjmBEkbW3cWCdps',
+    'txid'         : '1e49b7de8dd07eab030f53348641bd85f3df9f74d93231c62f36231ce2b0b7de',
+    'confirmations': 3,
+    'amount'       : 2,
+    'satoshis'     : 200000000,
+    'hash'         : '1e49b7de8dd07eab030f53348641bd85f3df9f74d93231c62f36231ce2b0b7de',
+    'pos'          : 0
+  }
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
+
+  describe('on testnet', () => {
+    it('get bitcoin utxos', async() => {
+      const resp = { data: [utxo] }
+      axios.get.mockResolvedValue(resp)
+
+      const rs = await insight.getUtxoList('msb86hf6ssyYkAJ8xqKUjmBEkbW3cWCdps', 'testnet')
+      expect(rs).toEqual([result])
+      expect(axios.get).toHaveBeenCalledTimes(1)
+      expect(axios.get).toHaveBeenCalledWith('https://test-insight.bitpay.com/api/addr/msb86hf6ssyYkAJ8xqKUjmBEkbW3cWCdps/utxo')
+    })
+  })
+
+  describe('on mainnet', () => {
+    it('get bitcoin utxos', async() => {
+      const resp = { data: [utxo] }
+      axios.get.mockResolvedValue(resp)
+
+      const rs = await insight.getUtxoList('msb86hf6ssyYkAJ8xqKUjmBEkbW3cWCdps', 'mainnet')
+      expect(rs).toEqual([result])
+      expect(axios.get).toHaveBeenCalledTimes(1)
+      expect(axios.get).toHaveBeenCalledWith('https://insight.bitpay.com/api/addr/msb86hf6ssyYkAJ8xqKUjmBEkbW3cWCdps/utxo')
+    })
+  })
+})
