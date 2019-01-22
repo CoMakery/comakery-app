66f1f56bd43c039121a38b12887cc0f75243ba63
Jashan Mudhar <jash.mud@outlook.com>
2019-01-23 00:12:57 +1000

    Migration issue

--- db/migrate/20190110044735_add_project_id_to_interests.rb
+++ db/migrate/20190110044735_add_project_id_to_interests.rb
@@ -1,6 +1,6 @@
 class AddProjectIdToInterests < ActiveRecord::Migration[5.1]
   def change
-    remove_column :interests, :project
+    remove_column :interests, :project, :string
     add_reference :interests, :project, index: true
   end
 end
