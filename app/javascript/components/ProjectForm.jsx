5e14a65c10f0b0e62000787902bf325ca9327bbe
Alex <no-reply@wwwwww.pw>
2019-02-19 19:48:50 +0300

    fix ProjectForm component

--- app/javascript/components/ProjectForm.jsx
+++ app/javascript/components/ProjectForm.jsx
@@ -304,7 +304,7 @@ class ProjectForm extends React.Component {
               title="total budget"
               recommended
               name="project[maximum_tokens]"
-              value={this.state['project[maximum_tokens]'] ? this.state['project[maximum_tokens]'].toString() : null}
+              value={this.state['project[maximum_tokens]'] ? this.state['project[maximum_tokens]'].toString() : ''}
               errorText={this.state.errors['project[maximum_tokens]']}
               placeholder="Provide the budget for completing the entire project"
               pattern="\d+"
