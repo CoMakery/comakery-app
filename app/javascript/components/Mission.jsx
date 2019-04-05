17ac184fffcef03e4778f918923395973fbafc50
Jashan Mudhar <jash.mud@outlook.com>
2019-04-06 03:35:08 +1000

    Css fix

--- app/javascript/components/Mission.jsx
+++ app/javascript/components/Mission.jsx
@@ -115,20 +115,43 @@ export default class Mission extends React.Component {
 
         <div className="mission-stats__container">
           <div className="mission-stats__kpis">
-            <div className="mission-stats__kpi">
+            <div className="mission-stats__kpis__row">
+              <div className="mission-stats__kpi">
+                <Icon name="PROJECT/WHITE.svg" />
+                <Pluralize singular="project" count={stats.projects} />
+              </div>
+              <div className="mission-stats__kpi">
+                <Icon name="BATCH/WHITE.svg" />
+                <Pluralize singular="batch" plural="batches" count={stats.batches} />
+              </div>
+            </div>
+
+            <div className="mission-stats__kpis__row">
+              <div className="mission-stats__kpi">
+                <Icon name="TASK/WHITE.svg" />
+                <Pluralize singular="task" count={stats.tasks} />
+              </div>
+              <div className="mission-stats__kpi">
+                <div className="mission-stats__contributor" />
+                <Pluralize singular="person" count={stats.interests} />&nbsp;INTERESTED
+              </div>
+            </div>
+
+            <div className="mission-stats__kpi mission-stats__kpi--desktop">
               <Icon name="PROJECT/WHITE.svg" />
               <Pluralize singular="project" count={stats.projects} />
             </div>
-            <div className="mission-stats__kpi">
+            <div className="mission-stats__kpi mission-stats__kpi--desktop">
               <Icon name="BATCH/WHITE.svg" />
               <Pluralize singular="batch" plural="batches" count={stats.batches} />
             </div>
-            <div className="mission-stats__kpi">
+
+            <div className="mission-stats__kpi mission-stats__kpi--desktop">
               <Icon name="TASK/WHITE.svg" />
               <Pluralize singular="task" count={stats.tasks} />
             </div>
-            <div className="mission-stats__kpi">
-              <Icon name="BATCH/WHITE.svg" />
+            <div className="mission-stats__kpi mission-stats__kpi--desktop">
+              <div className="mission-stats__contributor" />
               <Pluralize singular="person" count={stats.interests} />&nbsp;INTERESTED
             </div>
           </div>
@@ -145,30 +168,84 @@ export default class Mission extends React.Component {
                 <img src={project.tokenData.logoUrl} /> Paid in&nbsp;
                 <span className="mission-projects__single__token__name">{project.tokenData.name}</span>
               </div>
+
+              <div className="mission-projects__single__title-wrapper">
+                <div className="mission-projects__single__title">
+                  <div className="mission-projects__single__title__heading">{project.projectData.title}</div>
+                  {project.projectData.title}
+                </div>
+
+                <div className="mission-projects__single__contributors">
+                  <div className="project-contributors">
+                    <div className="project-leader">
+                      <div className="project-leader__info">
+                        <div className="project-leader__title">Team Leader</div>
+                        {project.projectData.teamLeader.firstName} {project.projectData.teamLeader.lastName}
+                      </div>
+                      <img className="project-leader__avatar" src={project.projectData.teamLeader.imageUrl} />
+                    </div>
+                    <div className="project-contributors__container">
+                      <img className="project-leader__avatar project-leader__avatar--mobile" src={project.projectData.teamLeader.imageUrl} />
+                      {project.projectData.contributors.map((contributor, index) =>
+                        <div key={contributor.id} className="project-contributor-container project-contributor-container--desktop">
+                          <img className="project-contributor__avatar" style={{zIndex: 5 - index}} src={contributor.imageUrl} />
+                          <div className="project-contributor__modal">
+                            <img className="project-contributor__modal-avatar" src={contributor.imageUrl} />
+                            <div className="project-contributor__modal__info">
+                              <div className="project-contributor__modal-nickname">{contributor.nickname && contributor.nickname}</div>
+                              <div className="project-contributor__modal-name">{contributor.firstName} {contributor.lastName}</div>
+                              <div className="project-contributor__modal-specialty">{contributor.specialty}</div>
+                            </div>
+                          </div>
+                        </div>
+                      )}
+                    </div>
+                    {project.projectData.contributorsNumber > 5 && <div className="project-contributors__more">+{project.projectData.contributorsNumber - 5}</div>}
+                    {project.projectData.contributorsNumber > 1 && <div className="project-contributors__more project-contributors__more--mobile">+{project.projectData.contributorsNumber - 1}</div>}
+                  </div>
+                </div>
+              </div>
             </div>
 
             <div className="mission-projects__single__card">
-              <div className="mission-projects__single__card__img">
-                <img style={{width: '100%'}} src={project.projectData.squareUrl || project.projectData.defaultImageUrl} />
-                <Icon className="mission-projects__single__card__img__mask" name="PROJECT/MASK.svg" />
-              </div>
-
-              <div className="mission-column__wrapper">
-                <div className="mission-projects__single__card__info">
-                  {project.projectData.description}
-                  <div className="mission-projects__single__card__info__stats">
-                    <div className="mission-projects__single__card__info__row">
-                      <Pluralize singular="batch" plural="batches" count={project.batches} />
-                      <Icon name="BATCH/GREY.svg" />
-                    </div>
-                    <div className="mission-projects__single__card__info__row">
-                      <Pluralize singular="task" count={project.tasks} />
-                      <Icon name="TASK/GRAY.svg" />
-                    </div>
-                  </div>
+              <div className="mission-projects__single__desktop">
+                <div className="mission-projects__single__card__img">
+                  <img style={{width: '100%'}} src={project.projectData.squareUrl || project.projectData.defaultImageUrl} />
+                  <Icon className="mission-projects__single__card__img__mask" name="PROJECT/MASK.svg" />
                 </div>
 
-                <div className="mission-projects__single__card__info__interest">
+                <div className="mission-column__wrapper">
+                  <div className="mission-projects__single__card__info">
+                    <span className="mission-projects__single__description--desktop">{project.projectData.description}</span>
+                    <div className="mission-projects__single__card__info__stats">
+                      <div className="mission-projects__single__card__info__row">
+                        <Pluralize singular="batch" plural="batches" count={project.batches} />
+                        <Icon name="BATCH/GREY.svg" />
+                      </div>
+                      <div className="mission-projects__single__card__info__row">
+                        <Pluralize singular="task" count={project.tasks} />
+                        <Icon name="TASK/GRAY.svg" />
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="mission-projects__single__card__info__interest">
+                    {project.editable && <a className="mission-projects__single__card__info__interest__link" href={`/projects/${project.projectData.id}/edit`}>EDIT PROJECT</a>}
+                    {!project.editable && interests[project.projectData.id] && <div className="mission-projects__single__card__info__interest__link" style={{opacity: 0.2}}>Request sent</div>}
+                    {!project.editable && !interests[project.projectData.id] &&
+                    <div
+                      className="mission-projects__single__card__info__interest__link"
+                      onClick={() => { this.addInterest(project.projectData.id) }}
+                    >I'M INTERESTED</div>
+                    }
+                    <a className="mission-projects__single__card__info__interest__link" href={`/projects/${project.projectData.id}`}>See details</a>
+                  </div>
+                </div>
+              </div>
+
+              <div className="mission-projects__single__mobile">
+                {project.projectData.description}
+                <div className="mission-projects__single__card__info__interest mission-projects__single__card__info__interest--mobile">
                   {project.editable && <a className="mission-projects__single__card__info__interest__link" href={`/projects/${project.projectData.id}/edit`}>EDIT PROJECT</a>}
                   {!project.editable && interests[project.projectData.id] && <div className="mission-projects__single__card__info__interest__link" style={{opacity: 0.2}}>Request sent</div>}
                   {!project.editable && !interests[project.projectData.id] &&
