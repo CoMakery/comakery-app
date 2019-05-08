7e912cdef22844d30c668c677e79f17fb19c1bd6
Alex <no-reply@wwwwww.pw>
2019-05-08 19:29:15 +0300

    rubocop

--- db/data_migrations/20190508153506_run_project_images_conversion.rb
+++ db/data_migrations/20190508153506_run_project_images_conversion.rb
@@ -1,5 +1,5 @@
 class RunProjectImagesConversion < ActiveRecord::DataMigration
   def up
-    raise 'Migration task did not succeed' unless system("rake migration:update_project_images")
+    raise 'Migration task did not succeed' unless system('rake migration:update_project_images')
   end
 end
