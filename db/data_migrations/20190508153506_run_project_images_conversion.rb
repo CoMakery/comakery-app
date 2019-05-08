1feddea2ed5fcda9488e0899809d7660ea344309
Alex <no-reply@wwwwww.pw>
2019-05-08 19:17:55 +0300

    add data migration to run project image conversion task

---
+++ db/data_migrations/20190508153506_run_project_images_conversion.rb
@@ -0,0 +1,5 @@
+class RunProjectImagesConversion < ActiveRecord::DataMigration
+  def up
+    raise 'Migration task did not succeed' unless system("rake migration:update_project_images")
+  end
+end
