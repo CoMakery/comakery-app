8aae202156eb348a7eb6b95d73aca39dc705ff09
Alex <no-reply@wwwwww.pw>
2019-06-03 17:28:46 +0300

    Fix uglify config

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,6 +1,8 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
 environment.loaders.delete('nodeModules')
 
 const config = environment.toWebpackConfig()
