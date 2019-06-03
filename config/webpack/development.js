713256b4cbd94b87f13db33d5be7f3ae6990d45f
Alex <no-reply@wwwwww.pw>
2019-06-03 17:22:24 +0300

    Upgrade uglify config, fix conflicts with wallet-related code

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,7 +1,32 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 environment.loaders.delete('nodeModules')
 
-module.exports = environment.toWebpackConfig()
+const config = environment.toWebpackConfig()
+
+config.optimization = {
+  minimizer: [
+    new UglifyJsPlugin({
+      uglifyOptions: {
+        mangle: {
+            reserved: [
+                'Buffer',
+                'BigInteger',
+                'Point',
+                'ECPubKey',
+                'ECKey',
+                'sha512_asm',
+                'asm',
+                'ECPair',
+                'HDNode'
+            ]
+        }
+      }
+    })
+  ]
+}
+
+module.exports = config
--- config/webpack/production.js
+++ config/webpack/production.js
@@ -3,4 +3,28 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 const environment = require('./environment')
 environment.loaders.delete('nodeModules')
 
-module.exports = environment.toWebpackConfig()
+const config = environment.toWebpackConfig()
+
+config.optimization = {
+  minimizer: [
+    new UglifyJsPlugin({
+      uglifyOptions: {
+        mangle: {
+            reserved: [
+                'Buffer',
+                'BigInteger',
+                'Point',
+                'ECPubKey',
+                'ECKey',
+                'sha512_asm',
+                'asm',
+                'ECPair',
+                'HDNode'
+            ]
+        }
+      }
+    })
+  ]
+}
+
+module.exports = config
