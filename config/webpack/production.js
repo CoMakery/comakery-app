6a1b8dda9a16509f58689e49f43e95e1f8711533
Alex <no-reply@wwwwww.pw>
2019-06-04 16:43:04 +0300

    Revert uglyfier config

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,32 +1,9 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 environment.loaders.delete('nodeModules')
 
 const config = environment.toWebpackConfig()
-
-config.optimization = {
-  minimizer: [
-    new UglifyJsPlugin({
-      uglifyOptions: {
-        mangle: {
-            reserved: [
-                'Buffer',
-                'BigInteger',
-                'Point',
-                'ECPubKey',
-                'ECKey',
-                'sha512_asm',
-                'asm',
-                'ECPair',
-                'HDNode'
-            ]
-        }
-      }
-    })
-  ]
-}
 
 module.exports = config
