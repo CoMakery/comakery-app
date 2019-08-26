02d74e0ece6ec83fa09a54f45251e9111c1c2385
Alex <no-reply@wwwwww.pw>
2019-08-27 00:31:10 +0200

    rubocop

--- db/data_migrations/20190826220709_make_nicknames_unique.rb
+++ db/data_migrations/20190826220709_make_nicknames_unique.rb
@@ -3,7 +3,7 @@
 
 class MakeNicknamesUnique < ActiveRecord::DataMigration
   def up
-    Account.group(:nickname).count.select{ |value, count| value && count > 1 }.keys.each do |nickname|
+    Account.group(:nickname).count.select { |value, count| value && count > 1 }.keys.each do |nickname|
       Account.where(nickname: nickname).each do |a|
         a.update_column(:nickname, "#{nickname} #{a.id}")
       end
