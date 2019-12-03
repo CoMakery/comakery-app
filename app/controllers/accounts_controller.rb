4af11a38eb4e4621761a2316582895ba31e89b64
Alex <no-reply@wwwwww.pw>
2019-12-03 19:46:48 +0300

    Revert datetime parsing fix

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -166,7 +166,7 @@ class AccountsController < ApplicationController
 
   def account_params
     result = params.require(:account).permit(:email, :ethereum_wallet, :qtum_wallet, :cardano_wallet, :bitcoin_wallet, :eos_wallet, :tezos_wallet, :first_name, :last_name, :nickname, :country, :date_of_birth, :image, :password, :specialty_id, :occupation, :linkedin_url, :github_url, :dribble_url, :behance_url)
-    result[:date_of_birth] = Date.parse(result[:date_of_birth]) if result[:date_of_birth].present?
+    result[:date_of_birth] = DateTime.strptime(result[:date_of_birth], '%m/%d/%Y') if result[:date_of_birth].present?
     result
   end
 
