748b96356e312dc8607cbb016e18857e520dd5e6
Alex <no-reply@wwwwww.pw>
2019-12-12 21:21:09 +0400

    Remove outdated migration

--- db/data_migrations/20190508153506_run_project_images_conversion.rb
+++
@@ -1,5 +0,0 @@
-class RunProjectImagesConversion < ActiveRecord::DataMigration
-  def up
-    raise 'Migration task did not succeed' unless system('rake migration:update_project_images')
-  end
-end
