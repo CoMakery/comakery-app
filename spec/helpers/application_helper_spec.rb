4cc3002ee9339809876ac5f714b26857556788d2
Alex C <no-reply@wwwwww.pw>
2020-10-03 23:40:08 +0300

    Fix transactions page
    - Update tx url link to use blockchain url helper
    - Remove unused code

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,12 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
-  describe 'ethereum_explorer_tx_url' do
-    it 'returns exporer url for a transaction' do
-      expect(helper.ethereum_explorer_tx_url(create(:token, _blockchain: 'ethereum_ropsten'), '123')).to eq('https://ropsten.etherscan.io/tx/123')
-    end
-  end
-
   describe 'ransack_filter_present?' do
     let!(:q) { Award.all.ransack(transfer_type_id_eq: '1202') }
 
