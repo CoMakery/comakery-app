790b74e9c5830f0484a2c694e7bab1b062579eb2
Alex C <no-reply@wwwwww.pw>
2020-11-04 16:18:10 +0300

    Update wallets UI
    - Show wallet state instead of addres if address is not present
    - Don't show remove button if wallet can not be removed

--- app/models/concerns/belongs_to_ore_id.rb
+++ app/models/concerns/belongs_to_ore_id.rb
@@ -4,10 +4,14 @@ module BelongsToOreId
   included do
     before_validation :create_ore_id, on: :create
     before_validation :pending_for_ore_id, on: :create
-    before_destroy :abort_destroy_for_id
+    before_destroy :abort_destroy_for_ore_id
 
     belongs_to :ore_id_account, optional: true
     validates :ore_id_account, presence: true, if: :ore_id?
+
+    def cannot_be_destroyed?
+      ore_id?
+    end
 
     private
 
@@ -19,9 +23,9 @@ module BelongsToOreId
         self.state = :pending if ore_id? && address.nil?
       end
 
-      def abort_destroy_for_id
-        if ore_id?
-          errors[:base] << 'An ORE ID wallet currently can not be deleted'
+      def abort_destroy_for_ore_id
+        if cannot_be_destroyed?
+          errors[:base] << 'An ORE ID wallet currently can not be removed'
           throw :abort
         end
       end
