84bc1d0008a13818eb36c00b9eca861d47041c8d
Sergey Andronov <riddlerrr@gmail.com>
2020-11-26 14:26:21 +0300

    Introduce BlockchainTransactionOptIn

--- app/models/blockchain_transaction_opt_in.rb
+++ app/models/blockchain_transaction_opt_in.rb
@@ -1,3 +1,10 @@
+# Algorand Standard Assets Opt-in transactions
 class BlockchainTransactionOptIn < BlockchainTransaction
-  belongs_to :token_opt_in
+  def update_transactable_status
+    blockchain_transactable.update!(status: :synced)
+  end
+
+  def on_chain
+    @on_chain ||= Comakery::Algorand.new(token.blockchain, contract_address)
+  end
 end
