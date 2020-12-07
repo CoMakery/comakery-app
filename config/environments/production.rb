c563dfa69b17029f02ef9436d1cceb4f48b9ef5f
Sergey Andronov <riddlerrr@gmail.com>
2020-12-07 13:22:51 +0300

    Production confing: Update SMTP settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,14 +96,14 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
-      :user_name => ENV['SENDGRID_USERNAME'],
-      :password => ENV['SENDGRID_PASSWORD'],
-      :domain => 'comakery.com',
-      :address => 'smtp.sendgrid.net',
-      :port => 587,
-      :authentication => :plain,
-      :enable_starttls_auto => true
+  config.action_mailer.smtp_settings = {
+    user_name: ENV['SENDGRID_USERNAME'] || ENV['SMTP_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'] || ENV['SMTP_PASSWORD'],
+    domain: ENV['SMTP_DOMAIN'] || ENV['SMTP_ADDRESS'] || 'comakery.com',
+    address: ENV['SMTP_ADDRESS'] || 'smtp.sendgrid.net',
+    port: ENV['SMTP_PORT'] || 587,
+    authentication: ENV['SMTP_PORT'] || :plain,
+    enable_starttls_auto: true
   }
 
   # Should be enabled ONLY on STAGING/DEMO env, since previews expose sensitive data
