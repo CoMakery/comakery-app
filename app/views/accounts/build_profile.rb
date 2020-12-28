0ab756eb9279ee6af83885074d170306b652a9e2
swimsoft <robertrehman72@gmail.com>
2020-12-28 13:32:46 -0800

    Change build profile page

--- app/views/accounts/build_profile.rb
+++ app/views/accounts/build_profile.rb
@@ -4,26 +4,10 @@ class Views::Accounts::BuildProfile < Views::Base
   def content
     row do
       column(%i[small-12 large-6], class: 'large-centered') do
-        h1('Build Your Profile')
+        h1('Setup Your Account')
 
         form_for account, url: update_profile_accounts_path do |f|
           row do
-            column('large-12') do
-              if skip_validation
-                label do
-                  text 'E-mail *'
-                  f.email_field :email
-                end
-              else
-                with_errors(account, :email) do
-                  label do
-                    text 'E-mail *'
-                    f.email_field :email
-                  end
-                end
-              end
-            end
-
             column('large-12') do
               with_errors(account, :first_name) do
                 label do
@@ -43,15 +27,6 @@ class Views::Accounts::BuildProfile < Views::Base
             end
 
             column('large-12') do
-              with_errors(account, :nickname) do
-                label do
-                  text 'Nickname'
-                  f.text_field :nickname
-                end
-              end
-            end
-
-            column('large-12') do
               with_errors(account, :date_of_birth) do
                 label do
                   text 'Date of Birth *'
@@ -64,62 +39,8 @@ class Views::Accounts::BuildProfile < Views::Base
               f.object.country ||= 'United States of America'
               with_errors(account, :country) do
                 label do
-                  text 'Country *'
+                  text 'Country of Citizenship *'
                   f.select :country, Country.all.sort
-                end
-              end
-            end
-
-            column('large-12') do
-              with_errors(account, :specialty) do
-                label do
-                  text 'What Is Your Specialty? *'
-                  f.collection_select :specialty_id, Specialty.all, :id, :name, include_blank: 'Please Select One'
-                end
-              end
-            end
-
-            column('large-12') do
-              with_errors(account, :occupation) do
-                label do
-                  text 'What Is Your Occupation?'
-                  f.text_field :occupation
-                end
-              end
-            end
-
-            column('large-12') do
-              with_errors(account, :linkedin_url) do
-                label do
-                  text 'LinkedIn Profile URL'
-                  f.text_field :linkedin_url
-                end
-              end
-            end
-
-            column('large-12') do
-              with_errors(account, :github_url) do
-                label do
-                  text 'GitHub Profile URL'
-                  f.text_field :github_url
-                end
-              end
-            end
-
-            column('large-12') do
-              with_errors(account, :dribble_url) do
-                label do
-                  text 'Dribble Profile URL'
-                  f.text_field :dribble_url
-                end
-              end
-            end
-
-            column('large-12') do
-              with_errors(account, :behance_url) do
-                label do
-                  text 'Behance Profile URL'
-                  f.text_field :behance_url
                 end
               end
             end
