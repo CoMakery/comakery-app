e4efa562b04ff1503f1093d82401ef0b160e1bf0
Ravi Singh <ravionrails@gmail.com>
2020-12-29 02:44:22 +0530

    commented non-existing methods from award migration

--- db/migrate/20170116023520_write_new_award_column_values.rb
+++ db/migrate/20170116023520_write_new_award_column_values.rb
@@ -1,7 +1,7 @@
 class WriteNewAwardColumnValues < ActiveRecord::Migration[4.2]
   def up
-    Award.reset_column_information
-    AwardType.write_all_award_amounts
+    # Award.reset_column_information
+    # AwardType.write_all_award_amounts
   end
 
   def down
