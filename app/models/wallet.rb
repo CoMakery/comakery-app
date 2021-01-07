15bfcaf1f58f06665c5acdfe618433aed743e83f
Mattias Jacobsson <matjacson914@gmail.com>
2021-01-07 20:05:30 +0200

    The first wallet is set as the primary wallet by default

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -10,16 +10,19 @@ class Wallet < ApplicationRecord
   validates :source, presence: true
   validates :address, presence: true, unless: :pending?
   validates :address, blockchain_address: true
-  validates :_blockchain, uniqueness: { scope: [:account_id, :primary_wallet], message: 'has primary wallet already' }, if: :primary_wallet?
+  validates :_blockchain, uniqueness: { scope: %i[account_id primary_wallet], message: 'has primary wallet already' }, if: :primary_wallet?
 
   attr_readonly :_blockchain
+
+  before_create :set_primary_flag
+  after_commit :mark_another_wallet_as_primary, on: [:destroy], if: :primary_wallet?
 
   enum source: { user_provided: 0, ore_id: 1 }
 
   def available_blockchains
     available_blockchains = Blockchain.available
     available_blockchains.reject!(&:supported_by_ore_id?)
-    available_blockchains.map(&:key) - account.wallets.pluck(:_blockchain)
+    available_blockchains.map(&:key)
   end
 
   def pending?
@@ -30,5 +33,20 @@ class Wallet < ApplicationRecord
     balance = balances.find_or_create_by(token: coin_of_the_blockchain)
     balance.update(base_unit_value: blockchain.account_coin_balance(address))
     balance
+  end
+
+  def set_primary_flag
+    self.primary_wallet = !Wallet.exists?(account_id: account_id, 
+                                          _blockchain: _blockchain, 
+                                          primary_wallet: true)
+  end
+
+  def mark_another_wallet_as_primary
+    first_wallet_in_network = Wallet
+                              .where(account_id: account_id, _blockchain: _blockchain)
+                              .order(id: :asc)
+                              .first
+
+    first_wallet_in_network&.update_column(:primary_wallet, true) # rubocop:disable Rails/SkipsModelValidations
   end
 end
