32a2038bd5d5a8ad5653b6d20e3f4897230e5049
Ravi Singh <ravionrails@gmail.com>
2021-01-08 17:10:13 +0530

    checking for conflict remove

--- db/migrate/20170116023520_write_new_award_column_values.rb
+++ db/migrate/20170116023520_write_new_award_column_values.rb
@@ -1,9 +1,8 @@
-class WriteNewAwardColumnValues < ActiveRecord::Migration[4.2]  
-  def up  
-    Award.reset_column_information  
-    AwardType.write_all_award_amounts 
-  end 
+class WriteNewAwardColumnValues < ActiveRecord::Migration[4.2]
+  def up
+    Award.reset_column_information
+  end
 
-  def down  
-  end 
+  def down
+  end
 end
