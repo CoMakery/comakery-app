3cf4a73ad24c2d92f0da70f8ae4f493cfda250e6
Ravi Singh <ravionrails@gmail.com>
2021-01-13 01:07:29 +0530

    changed whilable layput landing page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'pages#featured'
+  if ENV['WHITELABEL'] == "true"
+    root 'projects#index'
+  else
+    root 'pages#featured'
+  end
 
   resources :tokens, only: [:index, :new, :create, :show, :edit, :update] do
     collection do
