33048315e7b682618c7f7f3c433ff74170e7bcb0
Sergey Andronov <riddlerrr@gmail.com>
2021-01-14 08:53:17 +0100

    PopulateWalletNames data migration: Fix performance issue

--- db/data_migrations/20210108101936_populate_wallet_names.rb
+++ db/data_migrations/20210108101936_populate_wallet_names.rb
@@ -1,8 +1,9 @@
 class PopulateWalletNames < ActiveRecord::DataMigration
   def up
-    Wallet.find_each do |wallet|
-      wallet.name = wallet.blockchain.name
-      wallet.save
+    Wallet.select('array_agg(id) as ids, _blockchain').group(:_blockchain).each do |blockchain_wallets|
+      blockchain = "Blockchain::#{blockchain_wallets._blockchain.camelize}".constantize.new
+
+      Wallet.where(id: blockchain_wallets.ids).update_all(name: blockchain.name) # rubocop:todo Rails/SkipsModelValidations
     end
   end
 
