e924f488ea37534c5cd754c03ff80b5fd275017c
Ravi Singh <ravionrails@gmail.com>
2021-01-16 16:27:10 +0530

    removed layout condition from routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  if ENV['WHITELABEL'] == "true"
-    root 'projects#index'
-  else
+  # if ENV['WHITELABEL'] == "true"
+  #   root 'projects#index'
+  # else
     root 'pages#featured'
-  end
+  # end
 
   resources :tokens, only: [:index, :new, :create, :show, :edit, :update] do
     collection do
