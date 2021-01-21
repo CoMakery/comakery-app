57f369147965b7ab447e89bf34eb1a0efec21c28
Sergey Andronov <riddlerrr@gmail.com>
2021-01-21 13:23:46 +0100

    Add tests and some improvements

--- app/controllers/api/v1/wallet_recovery_controller.rb
+++ app/controllers/api/v1/wallet_recovery_controller.rb
@@ -2,8 +2,8 @@ class Api::V1::WalletRecoveryController < Api::V1::ApiController
   # TODO: Which authorization use here?
 
   def public_wrapping_key
-    public_key = Eth::Key.new(priv: ENV['WALLET_RECOVERY_WRAPPING_KEY']).public_key.key
-
+    private_key = ENV['WALLET_RECOVERY_WRAPPING_KEY'] || 'default_key'
+    public_key = Eth::Key.new(priv: private_key).public_key.key
     render json: { public_wrapping_key: public_key }
   rescue MoneyTree::Key::KeyFormatNotFound
     @errors = { invalid_env_variable: 'WALLET_RECOVERY_WRAPPING_KEY variable was not configured or use wrong format' }
---
+++ spec/controllers/api/v1/wallet_recovery_controller_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::WalletRecoveryController, type: :controller do
+  describe 'GET /public_wrapping_key' do
+    render_views
+
+    context 'with correct private key' do
+      # To check you can use https://gobittest.appspot.com/Address
+      it 'renders a successful response' do
+        ENV['WALLET_RECOVERY_WRAPPING_KEY'] = '18E14A7B6A307F426A94F8114701E7C8E774E7F9A47E2C2035DB29A206321725'
+        get :public_wrapping_key
+        expect(response).to be_successful
+        expect(response.body).to eq '{"public_wrapping_key":"0450863ad64a87ae8a2fe83c1af1a8403cb53f53e486d8511dad8a04887e5b23522cd470243453a299fa9e77237716103abc11a1df38855ed6f2ee187e9c582ba6"}'
+      end
+    end
+
+    context 'with incorrect private key' do
+      specify 'when private key is nil it renders an error' do
+        ENV['WALLET_RECOVERY_WRAPPING_KEY'] = nil
+        get :public_wrapping_key
+        expect(response.status).to eq 500
+        expect(response.body).to eq '{"errors":{"invalid_env_variable":"WALLET_RECOVERY_WRAPPING_KEY variable was not configured or use wrong format"}}'
+      end
+
+      specify 'when private key is blank string it renders an error' do
+        ENV['WALLET_RECOVERY_WRAPPING_KEY'] = ''
+        get :public_wrapping_key
+        expect(response.status).to eq 500
+        expect(response.body).to eq '{"errors":{"invalid_env_variable":"WALLET_RECOVERY_WRAPPING_KEY variable was not configured or use wrong format"}}'
+      end
+
+      specify 'when private key in wrong format it renders an error' do
+        ENV['WALLET_RECOVERY_WRAPPING_KEY'] = 'some_invalid_key'
+        get :public_wrapping_key
+        expect(response.status).to eq 500
+        expect(response.body).to eq '{"errors":{"invalid_env_variable":"WALLET_RECOVERY_WRAPPING_KEY variable was not configured or use wrong format"}}'
+      end
+    end
+  end
+end
