c21fb54bdf52d575d1d177e9c65bdad36da98c3c
Robert Rehman <robertrehman72@gmail.com>
2021-01-21 04:07:53 -0800

    Remove missions created by automation

---
+++ db/data_migrations/20210121113148_remove_missions_created_by_automation.rb
@@ -0,0 +1,8 @@
+class RemoveMissionsCreatedByAutomation < ActiveRecord::DataMigration
+  def up
+    if Rails.env.production?
+      name = Mission.arel_table[:name]
+      Mission.where(name.matches("%#{'Automation'}%")).destroy_all
+    end
+  end
+end
