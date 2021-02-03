dda8d66d723f7227e110adf1cbec223cb173794d
Sergey Andronov <riddlerrr@gmail.com>
2021-02-03 11:35:42 +0100

    HotWallet: Change storage to Redis

--- hotwallet/app.js
+++ hotwallet/app.js
@@ -1,45 +1,67 @@
 require('dotenv').config()
 const algosdk = require('algosdk')
-const fs = require('fs')
+const redis = require("redis");
 
 const projectId = process.env.PROJECT_ID
 const projectApiKey = process.env.PROJECT_API_KEY
 const comakeryServerUrl = process.env.COMAKERY_SERVER_URL
 const purestakeApi = process.env.PURESTAKE_API
+const redisUrl = process.env.REDIS_URL
+
+const keyName = "wallet_for_project_" + projectId
 
 function generateAlgorandKeyPair() {
   const account = algosdk.generateAccount()
   const mnemonic = algosdk.secretKeyToMnemonic(account.sk);
   const new_wallet = { address: account.addr, mnemonic: mnemonic }
 
-  console.log("address: " + new_wallet.address)
-  console.log( "Mnemonic: " + new_wallet.mnemonic )
-
   return new_wallet
 }
 
-function initialize() {
-  if (projectId === undefined || projectApiKey === undefined || comakeryServerUrl === undefined || purestakeApi === undefined) {
-    return "Some ENV vars was not set"
-  }
-  const key_filename = "wallet_for_project_" + projectId + ".key"
+function isAllVariablesSet() {
+  projectId === undefined || projectApiKey === undefined || comakeryServerUrl === undefined ||
+    purestakeApi === undefined || redisUrl === undefined
+}
 
-  if (fs.existsSync(key_filename)) {
-    return "wallet already created, do nothing..."
-  } else {
-    console.log("Key file does not exists, generating...")
-    var new_wallet = generateAlgorandKeyPair()
-    // TODO: call Comakery API to register the wallet and save file bellow on succesfull response
-    fs.writeFile(key_filename, JSON.stringify(new_wallet), 'utf8', function (err) {
-      if (err) {
-        return "Can't write into key file" + key_filename + "\n" + err
-      }
-    })
-  }
-  return null
+function initialize() {
+  if (isAllVariablesSet()) { return "Some ENV vars was not set" }
+
+  const redisClient = redis.createClient(redisUrl)
+  setRedisErrorHandler(redisClient)
+
+  redisClient.hgetall(keyName, function (err, wallet_keys) {
+    if (err) { return "Can't get a wallet keys: " + err }
+
+    if (wallet_keys) {
+      console.error("wallet already created, do nothing...")
+    } else {
+      console.log("Key file does not exists, generating...")
+      var new_wallet = generateAlgorandKeyPair()
+      // TODO: call Comakery API to register the wallet and save file bellow on succesfull response
+
+      redisClient.hset(keyName, "address", new_wallet.address, "mnemonic", new_wallet.mnemonic, function (err) {
+        return "Can't set a wallet key: " + err
+      })
+    }
+  })
+
+  return false // no errors
+}
+
+function setRedisErrorHandler(redisClient) {
+  redisClient.on("error", function (err) {
+    console.error("Redis client error: " + err);
+  });
+}
+
+function deleteCurrentKey() {
+  const redisClient = redis.createClient(redisUrl)
+  redisClient.del(keyName)
+  console.log("Wallet keys has been deleted: " + keyName)
 }
 
 (async () => {
+  // deleteCurrentKey()
   const res = initialize()
   if (res) { console.error(res); }
 })();
