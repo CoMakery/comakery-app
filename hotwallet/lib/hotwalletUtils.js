3326010d94f9e6dae5294f125583021322d223ad
Sergey Andronov <riddlerrr@gmail.com>
2021-02-08 11:44:45 +0100

    HW: move generateAlgorandKeyPair to utils

--- hotwallet/lib/hotwalletUtils.js
+++ hotwallet/lib/hotwalletUtils.js
@@ -1,4 +1,20 @@
+const algosdk = require('algosdk')
+
 exports.checkAllVariablesAreSet = function checkAllVariablesAreSet(envs) {
   return Boolean(envs.projectId) && Boolean(envs.projectApiKey) && Boolean(envs.comakeryServerUrl) &&
     Boolean(envs.purestakeApi) && Boolean(envs.redisUrl)
+}
+
+exports.setRedisErrorHandler = function setRedisErrorHandler(redisClient) {
+  redisClient.on("error", function (err) {
+    console.error(`Redis client error: ${err}`);
+  })
+}
+
+exports.generateAlgorandKeyPair = function generateAlgorandKeyPair() {
+  const account = algosdk.generateAccount()
+  const mnemonic = algosdk.secretKeyToMnemonic(account.sk);
+  const new_wallet = { address: account.addr, mnemonic: mnemonic }
+
+  return new_wallet
 }
