3cfb10e98754a920307068f9fb3a1f3a76f08c0a
Sergey Andronov <riddlerrr@gmail.com>
2021-02-08 15:43:21 +0100

    HW: Small housekeeping in tests

--- hotwallet/tests/registerHotWallet.test.js
+++ hotwallet/tests/registerHotWallet.test.js
@@ -8,6 +8,7 @@ const envs = {
   purestakeApi: "purestake_api_key",
   redisUrl: "redis://localhost:6379/0"
 }
+const wallet = { address: "YFGM3UODOZVHSI4HXKPXOKFI6T2YCIK3HKWJYXYFQBONJD4D3HD2DPMYW4", mnemonic: "mnemonic phrase" }
 
 jest.mock("axios")
 
@@ -19,8 +20,7 @@ afterAll(() => {
 
 test("return successfull response", async () => {
   expect.assertions(1);
-  const wallet = { address: "YFGM3UODOZVHSI4HXKPXOKFI6T2YCIK3HKWJYXYFQBONJD4D3HD2DPMYW4", mnemonic: "mnemonic phrase" }
-  axios.post.mockImplementation(() => Promise.resolve({ status: 201, data: { qwe: "qwe" } }))
+  axios.post.mockImplementation(() => Promise.resolve({ status: 201, data: {} }))
   res = await hwUtils.registerHotWallet(wallet, envs, redisClient)
 
   expect(res).toBe(true)
