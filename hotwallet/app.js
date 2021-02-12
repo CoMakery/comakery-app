8426603b39639548fbf5414553d8f4928e0384f6
Sergey Andronov <riddlerrr@gmail.com>
2021-02-12 14:17:20 +0100

    HW: Add chainjs and try to sign transaction

--- hotwallet/app.js
+++ hotwallet/app.js
@@ -1,6 +1,7 @@
-require('dotenv').config()
+require("dotenv").config()
 const redis = require("redis")
-const hwUtils = require('./lib/hotwalletUtils')
+const hwUtils = require("./lib/hotwalletUtils")
+const chainjs = require("@open-rights-exchange/chainjs")
 
 const envs = {
   projectId: process.env.PROJECT_ID,
@@ -11,6 +12,26 @@ const envs = {
 }
 
 const redisClient = redis.createClient(envs.redisUrl)
+
+const algoMainnetEndpoints = [{
+  url: 'https://mainnet-algorand.api.purestake.io/ps2',
+  options: { indexerUrl: 'https://mainnet-algorand.api.purestake.io/idx2', headers: [{ 'x-api-key': envs.purestakeApi }] },
+}]
+const algoTestnetEndpoints = [ {
+  url: 'https://testnet-algorand.api.purestake.io/ps2',
+  options: { indexerUrl: 'https://testnet-algorand.api.purestake.io/idx2', headers: [{ 'x-api-key': envs.purestakeApi }] },
+}]
+const algoBetanetEndpoints = [{
+  url: 'https://betanet-algorand.api.purestake.io/ps2',
+  options: { indexerUrl: 'https://betanet-algorand.api.purestake.io/idx2', headers: [{ 'x-api-key': envs.purestakeApi }] },
+}]
+const composeValueTransferParams = {
+  fromAccountName: "WDD3X6B4WFQDCH5N345I4F47LP5KCBI7YCDWFPJ2NO3YBNGQXBMYXT7T7U",
+  toAccountName: 'GD64YIY3TWGDMCNPP553DZPPR6LDUSFQOIJVFDPPXWEG3FVOJCCDBBHU5A',
+  amount: 100,
+  symbol: "microalgo",
+  memo: 'Hot wallet transactions',
+}
 
 async function initialize() {
   if (!hwUtils.checkAllVariablesAreSet(envs)) { return "Some ENV vars was not set" }
@@ -24,5 +45,23 @@ async function initialize() {
 (async () => {
   // await hwUtils.deleteCurrentKey(envs, redisClient)
   await initialize()
-  hwUtils.runServer(envs, redisClient)
+  // hwUtils.runServer(envs, redisClient)
+
+  const algoChain = new chainjs.ChainFactory().create(chainjs.ChainType.AlgorandV1, algoTestnetEndpoints)
+  await algoChain.connect()
+  if (algoChain.isConnected) {
+    console.log(`Connected to ${algoChain.chainId}`)
+  }
+
+  const transaction = await algoChain.new.Transaction()
+  const action = await algoChain.composeAction(chainjs.Models.ChainActionType.ValueTransfer, composeValueTransferParams)
+  transaction.actions = [action]
+  console.log('transaction actions: ', transaction.actions[0])
+  const decomposed = await algoChain.decomposeAction(transaction.actions[0])
+  console.log('decomposed actions: ', decomposed)
+  const suggestedFee = await transaction.getSuggestedFee(chainjs.Models.TxExecutionPriority.Average)
+  console.log('suggestedFee: ', suggestedFee)
+  await transaction.setDesiredFee(suggestedFee)
+  await transaction.prepareToBeSigned()
+  await transaction.validate()
 })();
