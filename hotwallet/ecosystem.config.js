e145bdc796d372b16b64c36695295d0b26189baf
Sergey Andronov <riddlerrr@gmail.com>
2021-02-18 11:33:36 +0100

    HW: Add PM2 config for heroku deployment

---
+++ hotwallet/ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps : [{
+    name: "HotWallet",
+    script: './app.js',
+    instances: 1,
+    env: {
+      NODE_ENV: "development",
+    },
+    env_production: {
+      NODE_ENV: "production",
+    }
+  }],
+
+  deploy : {
+    production : {
+      'post-deploy' : 'yarn install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
