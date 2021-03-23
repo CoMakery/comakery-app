1630c21c9230fe7dafab5747f4421e7a8ca8af2f
Zh Infuhnya <zhinfuhnya@gmail.com>
2021-03-23 21:46:51 +0200

    Update register spec

--- spec/interactors/accounts/register_spec.rb
+++ spec/interactors/accounts/register_spec.rb
@@ -2,7 +2,22 @@ require 'rails_helper'
 
 describe Accounts::Register do
   describe '#call' do
-    context 'when data is valid' do
+    context 'when the whitelabel mission is present' do
+      let!(:whitelabel_mission) { create(:whitelabel_mission) }
+      let!(:account_params) { { email: 'example@gmail.com', password: 'password', agreed_to_user_agreement: '1' } }
+
+      subject(:result) do
+        described_class.call(whitelabel_mission: whitelabel_mission, account_params: account_params)
+      end
+
+      it 'creates whitelabel account' do
+        expect(result.success?).to eq(true)
+
+        expect(result.account.managed_account_id).not_to be(nil)
+      end
+    end
+
+    context 'when the whitelabel mission is nil' do
       let!(:account_params) { { email: 'example@gmail.com', password: 'password', agreed_to_user_agreement: '1' } }
 
       subject(:result) do
