dcccd51b276e81d12700dc6b251a55290422e697
Zh Infuhnya <zhinfuhnya@gmail.com>
2021-03-29 22:04:41 +0300

    Return bad request status for non ore id wallets

--- spec/controllers/api/v1/wallets_controller_spec.rb
+++ spec/controllers/api/v1/wallets_controller_spec.rb
@@ -229,7 +229,11 @@ RSpec.describe Api::V1::WalletsController, type: :controller do
       let(:wallet) { create(:ore_id_wallet, account: account) }
 
       it 'returns url for password reset and scedules a job for password' do
-        params = build(:api_signed_request, { redirect_url: 'https://localhost' }, password_reset_api_v1_account_wallet_path(account_id: account.managed_account_id, id: wallet.id.to_s), 'POST')
+        params = build(:api_signed_request,
+                       { redirect_url: 'https://localhost' },
+                       password_reset_api_v1_account_wallet_path(account_id: account.managed_account_id,
+                                                                 id: wallet.id.to_s),
+                       'POST')
         params[:account_id] = account.managed_account_id
         params[:id] = wallet.id
 
@@ -257,6 +261,26 @@ RSpec.describe Api::V1::WalletsController, type: :controller do
           'hmac' => build(:ore_id_hmac, parsed_response['reset_url'], url_encode: false)
         )
         expect(wallet.ore_id_account.reload.state).to eq 'unclaimed'
+      end
+    end
+
+    context 'when non ore id wallet' do
+      render_views
+      let(:wallet) { create(:wallet, account: account) }
+
+      it 'returns url for password reset and scedules a job for password' do
+        params = build(:api_signed_request,
+                       { redirect_url: 'https://localhost' },
+                       password_reset_api_v1_account_wallet_path(account_id: account.managed_account_id,
+                                                                 id: wallet.id.to_s),
+                       'POST')
+        params[:account_id] = account.managed_account_id
+        params[:id] = wallet.id
+
+        post :password_reset, params: params
+
+        expect(response).to have_http_status(:bad_request)
+        expect(JSON.parse(response.body)['errors']['ore_id_account']).to eq('can\'t be blank')
       end
     end
   end
