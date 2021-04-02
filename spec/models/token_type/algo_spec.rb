4b8fb0295f253882727041c91df437630e8409f0
Sergey Andronov <riddlerrr@gmail.com>
2021-04-02 13:04:34 +0200

    Fix algo and eth token_type specs

--- spec/models/token_type/algo_spec.rb
+++ spec/models/token_type/algo_spec.rb
@@ -8,7 +8,7 @@ describe TokenType::Algo do
   specify { expect(described_class.new.symbol).to eq('ALGO') }
   specify { expect(described_class.new.decimals).to eq(6) }
   specify { expect(described_class.new.wallet_logo).to eq('OREID_Logo_Symbol.svg') }
-  specify { expect(described_class.new.contract).to be_nil }
+  specify { expect(described_class.new.contract).to be_a(Comakery::Algorand) }
   specify { expect(described_class.new.abi).to eq({}) }
   specify { expect(described_class.new.tx).to be_nil }
   specify { expect(described_class.new.operates_with_smart_contracts?).to be_falsey }
--- spec/models/token_type/eth_spec.rb
+++ spec/models/token_type/eth_spec.rb
@@ -4,18 +4,20 @@ require 'models/token_type_spec'
 describe TokenType::Eth do
   it_behaves_like 'a token type'
 
-  specify { expect(described_class.new.name).to eq('ETH') }
-  specify { expect(described_class.new.symbol).to eq('ETH') }
-  specify { expect(described_class.new.decimals).to eq(18) }
-  specify { expect(described_class.new.wallet_logo).to eq('metamask2.png') }
-  specify { expect(described_class.new.contract).to be_nil }
-  specify { expect(described_class.new.abi).to eq({}) }
-  specify { expect(described_class.new.tx).to eq(Comakery::Eth::Tx) }
-  specify { expect(described_class.new.operates_with_smart_contracts?).to be_falsey }
-  specify { expect(described_class.new.operates_with_account_records?).to be_falsey }
-  specify { expect(described_class.new.operates_with_reg_groups?).to be_falsey }
-  specify { expect(described_class.new.operates_with_transfer_rules?).to be_falsey }
-  specify { expect(described_class.new.supports_token_mint?).to be_falsey }
-  specify { expect(described_class.new.supports_token_burn?).to be_falsey }
-  specify { expect(described_class.new.supports_token_freeze?).to be_falsey }
+  let(:attrs) { { contract_address: '0x583cbbb8a8443b38abcc0c956bece47340ea1367', blockchain: Blockchain::EthereumRopsten.new } }
+
+  specify { expect(described_class.new(**attrs).name).to eq('ETH') }
+  specify { expect(described_class.new(**attrs).symbol).to eq('ETH') }
+  specify { expect(described_class.new(**attrs).decimals).to eq(18) }
+  specify { expect(described_class.new(**attrs).wallet_logo).to eq('metamask2.png') }
+  specify { expect(described_class.new(**attrs).contract).to be_a(Comakery::Eth) }
+  specify { expect(described_class.new(**attrs).abi).to eq({}) }
+  specify { expect(described_class.new(**attrs).tx).to eq(Comakery::Eth::Tx) }
+  specify { expect(described_class.new(**attrs).operates_with_smart_contracts?).to be_falsey }
+  specify { expect(described_class.new(**attrs).operates_with_account_records?).to be_falsey }
+  specify { expect(described_class.new(**attrs).operates_with_reg_groups?).to be_falsey }
+  specify { expect(described_class.new(**attrs).operates_with_transfer_rules?).to be_falsey }
+  specify { expect(described_class.new(**attrs).supports_token_mint?).to be_falsey }
+  specify { expect(described_class.new(**attrs).supports_token_burn?).to be_falsey }
+  specify { expect(described_class.new(**attrs).supports_token_freeze?).to be_falsey }
 end
