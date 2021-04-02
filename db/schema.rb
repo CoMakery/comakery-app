ceccc715907dc6fe650a6314cc1383ef9d433d2f
Sergey Andronov <riddlerrr@gmail.com>
2021-04-02 12:13:52 +0200

    Balance: Change base_unit_value type to decimal

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_22_081202) do
+ActiveRecord::Schema.define(version: 2021_04_02_100522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -263,7 +263,7 @@ ActiveRecord::Schema.define(version: 2021_03_22_081202) do
   create_table "balances", force: :cascade do |t|
     t.bigint "wallet_id"
     t.bigint "token_id"
-    t.bigint "base_unit_value", default: 0, null: false
+    t.decimal "base_unit_value", precision: 40, default: "0", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["token_id"], name: "index_balances_on_token_id"
