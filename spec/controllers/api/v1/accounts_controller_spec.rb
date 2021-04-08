b08d20e89a3b94f7c7d51f624f49edda2dfc5c15
Sergey Andronov <riddlerrr@gmail.com>
2021-04-08 12:42:49 +0200

    debug CI failure

--- spec/controllers/api/v1/accounts_controller_spec.rb
+++ spec/controllers/api/v1/accounts_controller_spec.rb
@@ -105,9 +105,9 @@ RSpec.describe Api::V1::AccountsController, type: :controller do
       it 'add interested projects' do
         project1 = create(:project, mission: active_whitelabel_mission)
         project2 = create(:project, mission: active_whitelabel_mission)
-        # ignored_project = create(:project)
+        ignored_project = create(:project)
 
-        # valid_attributes[:project_interests] = [project1.id.to_s, project2.id.to_s, ignored_project.id.to_s]
+        valid_attributes[:project_interests] = [project1.id.to_s, project2.id.to_s, ignored_project.id.to_s]
         valid_attributes[:project_interests] = [project1.id.to_s, project2.id.to_s]
         params = build(:api_signed_request, { account: valid_attributes }, api_v1_accounts_path, 'POST')
         post :create, params: params
