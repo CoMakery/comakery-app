78ad0dc9a761b769456e32710d4effa826ae24e0
Sergey Andronov <riddlerrr@gmail.com>
2021-04-08 19:22:59 +0200

    Try to debug CI

--- spec/controllers/api/v1/accounts_controller_spec.rb
+++ spec/controllers/api/v1/accounts_controller_spec.rb
@@ -103,16 +103,20 @@ RSpec.describe Api::V1::AccountsController, type: :controller do
 
     context 'with project_interests param' do
       it 'add interested projects' do
+        # active_whitelabel_mission = create(:active_whitelabel_mission)
         project1 = create(:project, mission: active_whitelabel_mission)
         project2 = create(:project, mission: active_whitelabel_mission)
         ignored_project = create(:project)
 
         valid_attributes[:project_interests] = [project1.id.to_s, project2.id.to_s, ignored_project.id.to_s]
         params = build(:api_signed_request, { account: valid_attributes }, api_v1_accounts_path, 'POST')
-        post :create, params: params
 
+        pp active_whitelabel_mission.reload
+        sleep 1
+
+        post :create, params: params
         account = Account.last
-        expect(account.projects_interested.reload.pluck(:id)).to eq [project1.id, project2.id]
+        expect(account.projects_interested.pluck(:id)).to eq [project1.id, project2.id]
       end
 
       it 'with empty array provided' do
