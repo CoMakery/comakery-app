714d35a18e278b5c22f4833df51957d2577f6a2e
Sergey Andronov <riddlerrr@gmail.com>
2021-04-12 13:36:39 +0200

    Refactor a condition to be more human readable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -240,7 +240,7 @@ class ApplicationController < ActionController::Base
   end
 
   def redirect_to_default_whitelabel_domain
-    return unless ENV['WHITELABEL'] == 'true'
+    return if ENV['WHITELABEL'] != 'true'
     return if @whitelabel_mission
 
     redirect_to root_url(host: ENV['APP_HOST'])
