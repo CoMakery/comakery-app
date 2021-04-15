fb88de507102a29fd91344ee948b6883fe35d955
Oleksiy Bahriy <obahriy@leobit.com>
2021-04-15 14:30:09 +0300

    removed oreid from wallets index

--- app/helpers/wallets_helper.rb
+++ app/helpers/wallets_helper.rb
@@ -2,4 +2,8 @@ module WalletsHelper
   def wallet_blockchain_collection(wallet)
     wallet.available_blockchains.map { |k| [Wallet.blockchain_for(k).name, k] }
   end
+
+  def ore_id_configured
+    ENV['ORE_ID_API_KEY'] && ENV['ORE_ID_SERVICE_KEY']
+  end
 end
