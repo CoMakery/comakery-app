627f7d324fb1bfcd4e0bf0093ae4c8966ac34c4c
Zh Infuhnya <zhinfuhnya@gmail.com>
2021-04-15 17:44:05 +0300

    Align in a single-line

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -145,9 +145,7 @@ def click_remove(award_type_row)
 end
 
 def wait_for_turbolinks
-  if has_css?('.turbo-progress-bar', visible: true, wait: 1.second)
-    has_no_css?('.turbo-progress-bar', wait: 5.seconds)
-  end
+  has_no_css?('.turbo-progress-bar', wait: 5.seconds) if has_css?('.turbo-progress-bar', visible: true, wait: 1.second)
 end
 
 include SlackStubs
