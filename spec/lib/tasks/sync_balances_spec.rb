783fe1e8b97cdcbb0f620b7347d6ad7c824bb95b
Sergey Andronov <riddlerrr@gmail.com>
2021-04-15 12:41:32 +0200

    Fix spec

--- spec/lib/tasks/sync_balances_spec.rb
+++ spec/lib/tasks/sync_balances_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'rake balances:sync_all', type: :task do
   it 'runs the sync job' do
-    expect(SyncBalancesJob).to receive(:perform_now)
+    expect(SyncBalancesJob).to receive(:perform_now).at_least(:once)
     task.execute
   end
 end
