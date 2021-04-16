7d833980ca12008e35b03a0d2306433718d0ee3f
Sergey Andronov <riddlerrr@gmail.com>
2021-04-16 10:26:43 +0200

    Freeze time for SyncBalanceJob spec

--- spec/jobs/sync_balance_job_spec.rb
+++ spec/jobs/sync_balance_job_spec.rb
@@ -4,6 +4,11 @@ RSpec.describe SyncBalanceJob, type: :job do
   let(:balance) { create(:balance) }
   subject { described_class.perform_now(balance) }
 
+  before do
+    # Freeze time, replace me with Timecop.freeze
+    allow_any_instance_of(ActiveSupport::TimeZone).to receive(:now).and_return(Time.zone.now)
+  end
+
   it 'succesfully update a value' do
     allow_any_instance_of(Balance).to receive(:blockchain_balance_base_unit_value).and_return(999)
     is_expected.to be true
@@ -11,7 +16,7 @@ RSpec.describe SyncBalanceJob, type: :job do
   end
 
   context 'do not update a value' do
-    let(:balance) { create(:balance, created_at: 1.day.ago, base_unit_value: 10) }
+    let(:balance) { create(:balance, created_at: 1.day.ago, updated_at: Time.zone.now, base_unit_value: 10) }
 
     it 'when it was updated recently' do
       allow_any_instance_of(Balance).to receive(:blockchain_balance_base_unit_value).and_return(999)
