73a126b945a1a8bdfd076d90e752ed9f06a1dc86
Oleksiy Bahriy <obahriy@leobit.com>
2021-04-20 13:19:09 +0300

    moved check to blockchain rb

--- app/helpers/wallets_helper.rb
+++ app/helpers/wallets_helper.rb
@@ -2,8 +2,4 @@ module WalletsHelper
   def wallet_blockchain_collection(wallet)
     wallet.available_blockchains.map { |k| [Wallet.blockchain_for(k).name, k] }
   end
-
-  def ore_id_configured
-    ENV['ORE_ID_API_KEY'] && ENV['ORE_ID_SERVICE_KEY']
-  end
 end
