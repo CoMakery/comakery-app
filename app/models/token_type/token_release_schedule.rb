14842a022e591f34de16ebbbb6b4842db7280bcb
Oleksiy Bahriy <obahriy@leobit.com>
2021-04-21 20:28:52 +0300

    changed token type

--- app/models/token_type/token_release_schedule.rb
+++ app/models/token_type/token_release_schedule.rb
@@ -1,94 +1,94 @@
-class TokenType::TokenReleaseSchedule < TokenType
+class TokenType::TokenReleaseSchedule < TokenType::Erc20
   # Generated template for implementing a new token type subclass
   # See: rails g token_type -h
 
   # Name of the token type for UI purposes
   # @return [String] name
   def name
-    'TOKENRELEASESCHEDULE'
+    'Token Release Schedule'
   end
 
   # Symbol of the token type for UI purposes
   # @return [String] symbol
   def symbol
-    'TOKENRELEASESCHEDULE'
+    super
   end
 
   # Number of decimals
   # @return [Integer] number
   def decimals
-    0
+    super
   end
 
   # Wallet logo filename for UI purposes (relative to `app/assets/images`)
   # @return [String] filename
   def wallet_logo
-    nil
+    super
   end
 
   # Contract instance if implemented
   # @return [nil]
   def contract
-    # Comakery::Eth::Contract::Erc20.new.new
+    super
   end
 
   # ABI structure if present
   # @return [Hash] abi
   def abi
-    {}
+    super
   end
 
   # Transaction instance if implemented
   # @return [nil]
   def tx
-    # Comakery::Eth::Tx.new.new
+    super
   end
 
   # Does it have support for smart contracts?
   # @return [Boolean] flag
   def operates_with_smart_contracts?
-    false
+    super
   end
 
   # Does it have custom account data stored on chain?
   # @return [Boolean] flag
   def operates_with_account_records?
-    false
+    super
   end
 
   # Does it have support for account groups?
   # @return [Boolean] flag
   def operates_with_reg_groups?
-    false
+    super
   end
 
   # Does it have support for transfer restrictions between accounts/groups?
   # @return [Boolean] flag
   def operates_with_transfer_rules?
-    false
+    super
   end
 
   # Does it have support for minting new tokens to a custom address?
   # @return [Boolean] flag
   def supports_token_mint?
-    false
+    super
   end
 
   # Does it have support for burning existing tokens from a custom address?
   # @return [Boolean] flag
   def supports_token_burn?
-    false
+    super
   end
 
   # Does it have support for temporal freezing of all token transactions?
   # @return [Boolean] flag
   def supports_token_freeze?
-    false
+    super
   end
 
   # Return balance of symbol for provided addr
   # @return [Integer] balance
   def blockchain_balance(_wallet_address)
-    raise NotImplementedError
+    super
   end
 end
