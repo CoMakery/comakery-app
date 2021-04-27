d5dbf5bbeda5c22283cc961e11f2a2e709fec646
Zh Infuhnya <zhinfuhnya@gmail.com>
2021-04-27 13:06:06 +0300

    Add specs

---
+++ spec/controllers/projects/invites_controller.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+RSpec.describe Projects::InvitesController, type: :controller do
+  let!(:admin) { create(:account, comakery_admin: true) }
+  let!(:project) { create(:project, account: admin) }
+  let!(:account) { create(:account, email: 'example@gmail.com') }
+
+  let!(:params) do
+    {
+      project_id: project.id,
+      email: 'example@gmail.com',
+      role: :member
+    }
+  end
+
+  before do
+    login(project.account)
+  end
+
+  describe 'POST #create' do
+    context 'with valid params' do
+      it 'creates a new interest' do
+        expect do
+          post :create, params: params, format: :json
+        end.to change(Interest, :count).by(1)
+      end
+
+      it 'returns a success response' do
+        post :create, params: params, format: :json
+        expect(response).to have_http_status(:created)
+      end
+    end
+
+    context 'with invalid params' do
+      let!(:params) do
+        {
+          project_id: project.id,
+          email: 'unregister@gmail.com',
+          role: :member
+        }
+      end
+
+      it 'respond with errors' do
+        post :create, params: params, format: :json
+        expect(JSON.parse(response.body)['errors']).to eq(['The user must have signed up to add them'])
+      end
+
+      it 'returns a fail response' do
+        post :create, params: params, format: :json
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
+end
---
+++ spec/features/dashboard/accesses/add_person_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+describe 'Add person', js: true do
+  let(:account) { create(:account, comakery_admin: true) }
+  let(:owner) { create(:account, comakery_admin: true) }
+  let(:project) { create(:project, account: owner) }
+
+  scenario 'grants access to registered account' do
+    login(owner)
+    visit project_dashboard_accesses_path(project)
+
+    find('[data-target="#invite-person"]').click
+
+    within('#invite-person form') do
+      fill_in 'email', with: account.email
+
+      select 'Member', from: 'role'
+
+      click_button 'Save'
+    end
+
+    expect(find('.flash-message-container')).to have_content('Invite successfully sent')
+  end
+
+  scenario 'fails with unregistered account' do
+    login(owner)
+    visit project_dashboard_accesses_path(project)
+
+    find('a[data-target="#invite-person"]').click
+
+    within('#invite-person form') do
+      fill_in 'email', with: ''
+
+      select 'Member', from: 'role'
+
+      click_button 'Save'
+    end
+
+    expect(find('#invite-person ul.errors')).to have_content('The user must have signed up to add them')
+  end
+end
