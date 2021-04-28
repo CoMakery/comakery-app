b3982ea5ce0932d33be3086d411c308d6c75ad73
Alex C <no-reply@wwwwww.pw>
2021-04-28 03:03:53 +0300

    Fix specs

--- spec/jobs/sync_balances_job_spec.rb
+++ spec/jobs/sync_balances_job_spec.rb
@@ -1,18 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe SyncBalancesJob, type: :job do
-  let!(:balances) do
-    [
-      create(:balance, created_at: 11.seconds.ago, updated_at: 11.seconds.ago),
-      create(:balance, created_at: 11.seconds.ago, updated_at: 11.seconds.ago),
-      create(:balance, created_at: 1.second.ago, updated_at: 1.second.ago)
-    ]
-  end
+  let(:token_with_balance_support) { create(:comakery_token) }
+  let(:wallet) { create(:wallet, address: build(:ethereum_address_1), _blockchain: token_with_balance_support._blockchain) }
+
   subject { described_class.perform_now }
 
-  it 'schedule sync balance jobs for required balances' do
-    expect(SyncBalanceJob).to receive(:perform_later).twice
+  context 'when balance has not been updated after creation' do
+    let!(:balance) { create(:balance, wallet: wallet, token: token_with_balance_support) }
 
-    is_expected.to be true
+    it 'schedules sync balance job' do
+      expect(SyncBalanceJob).to receive(:perform_later)
+
+      subject
+    end
+  end
+
+  context 'when balance has been updated long time ago' do
+    let!(:balance) { create(:balance, wallet: wallet, token: token_with_balance_support, updated_at: 1.year.ago) }
+
+    it 'schedules sync balance job' do
+      expect(SyncBalanceJob).to receive(:perform_later)
+
+      subject
+    end
+  end
+
+  context 'when balance has been just updated' do
+    let!(:balance) { create(:balance, wallet: wallet, token: token_with_balance_support, updated_at: 1.year.from_now) }
+
+    it 'doesnt schedule sync balance job' do
+      expect(SyncBalanceJob).not_to receive(:perform_later)
+
+      subject
+    end
   end
 end
