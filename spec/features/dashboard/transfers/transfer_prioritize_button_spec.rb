d92283500f3244cd8612cff2e54d3c78e7f8488c
Zh Infuhnya <zhinfuhnya@gmail.com>
2021-04-30 15:46:46 +0300

    Add hot wallet mode to stream for transfer prioritize btn mode

--- spec/features/dashboard/transfers/transfer_prioritize_button_spec.rb
+++ spec/features/dashboard/transfers/transfer_prioritize_button_spec.rb
@@ -9,7 +9,7 @@ describe 'transfer prioritize button on transfers page', js: true do
 
   before { create(:wallet, source: :hot_wallet, project_id: project.id) }
 
-  before { project.update!(hot_wallet_mode: :auto_sending) }
+  before { project.update(hot_wallet_mode: :auto_sending) }
 
   before { transfer.accepted! }
 
@@ -41,10 +41,22 @@ describe 'transfer prioritize button on transfers page', js: true do
     context 'with succeed status' do
       before { blockchain_transaction.update(status: :succeed) }
 
-      it 'is present' do
-        expect(page).to have_css('.transfers-table__transfer__button__history #prioritizeBtn', count: 1)
+      context 'when hot wallet mode is auto sending' do
+        it { expect(page).to have_css('.transfers-table__transfer__button__history #prioritizeBtn', count: 1) }
 
-        expect(find('.transfers-table__transfer__button__history #prioritizeBtn').text).to eq('PRIORITIZE')
+        it 'has link to prioritize' do
+          expect(find('.transfers-table__transfer__button__history #prioritizeBtn').text).to eq('PRIORITIZE')
+        end
+      end
+
+      context 'when hot wallet mode is manual sending' do
+        before { project.update(hot_wallet_mode: :manual_sending) }
+
+        it { expect(page).to have_css('.transfers-table__transfer__button__history #prioritizeBtn', count: 1) }
+
+        it 'has link to pay by hot wallet' do
+          expect(find('.transfers-table__transfer__button__history #prioritizeBtn').text).to eq('PAY BY HOT WALLET')
+        end
       end
     end
   end
