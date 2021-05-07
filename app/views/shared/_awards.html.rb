da335d85a40eb0e08a28bdde8649a67563daa424
olehyev138 <oleg.ye@yahoo.com>
2021-05-08 00:03:19 +0300

    issue fix

--- app/views/shared/_awards.html.rb
+++ app/views/shared/_awards.html.rb
@@ -64,11 +64,6 @@ class Views::Shared::Awards < Views::Base
                   link_to '(no account)', account_path
                 elsif award.recipient_address.blank?
                   text '(no account)'
-                elsif current_account&.decorate?(project)
-                  link_to 'javascript:void(0)', class: 'metamask-transfer-btn transfer-tokens-btn', 'data-id': award.id do
-                    span 'Send'
-                    wallet_logo
-                  end
                 else
                   text '(pending)'
                 end
