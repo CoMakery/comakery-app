ce39bfb6e83dce2ed818c4d1eb64a8e29172478f
olehyev138 <oleg.ye@yahoo.com>
2021-05-10 23:42:57 +0300

    Update Transfer columns

--- spec/features/dashboard/transfers/transfer_button_spec.rb
+++ spec/features/dashboard/transfers/transfer_button_spec.rb
@@ -267,12 +267,12 @@ describe 'transfer button on Transfers page' do
           end
 
           it 'links to WalletConnect and Metamask controllers' do
-            expect(page).to have_css('.transfers-table__transfer__button .transfer-button', count: 1, text: 'Pay')
-            expect(page).to have_css('.transfers-table__transfer__button .transfer-button a[data-action="click->sign--wallet-connect#sendTx click->sign--metamask#sendTx"]', count: 1)
-            expect(page).to have_css('.transfers-table__transfer__button .transfer-button a[data-sign--wallet-connect-target="txButtons"]', count: 1)
-            expect(page).to have_css('.transfers-table__transfer__button .transfer-button a[data-sign--metamask-target="txButtons"]', count: 1)
-            expect(page).to have_css('.transfers-table__transfer__button .transfer-button a[data-tx-new-url^=\/]', count: 1)
-            expect(page).to have_css('.transfers-table__transfer__button .transfer-button a[data-tx-receive-url^=\/]', count: 1)
+            expect(page).to have_css('.transfers-table__transfer__status .transfer-button', count: 1, text: 'Pay')
+            expect(page).to have_css('.transfers-table__transfer__status .transfer-button a[data-action="click->sign--wallet-connect#sendTx click->sign--metamask#sendTx"]', count: 1)
+            expect(page).to have_css('.transfers-table__transfer__status .transfer-button a[data-sign--wallet-connect-target="txButtons"]', count: 1)
+            expect(page).to have_css('.transfers-table__transfer__status .transfer-button a[data-sign--metamask-target="txButtons"]', count: 1)
+            expect(page).to have_css('.transfers-table__transfer__status .transfer-button a[data-tx-new-url^=\/]', count: 1)
+            expect(page).to have_css('.transfers-table__transfer__status .transfer-button a[data-tx-receive-url^=\/]', count: 1)
           end
 
           it 'links to WalletConnect controller' do
