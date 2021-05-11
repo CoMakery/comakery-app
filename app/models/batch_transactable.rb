9e8fdbf01794689e7fdc9ed63bc65c5ae4f4b930
Sergey Andronov <riddlerrr@gmail.com>
2021-05-11 13:33:46 +0200

    Clean up

--- app/models/batch_transactable.rb
+++ app/models/batch_transactable.rb
@@ -1,6 +1,4 @@
 class BatchTransactable < ApplicationRecord
   belongs_to :transaction_batch
   belongs_to :blockchain_transactable, polymorphic: true
-
-  scope :prioritization_support, -> { where(blockchain_transactable_type: %w[Award AccountTokenRecord]) }
 end
