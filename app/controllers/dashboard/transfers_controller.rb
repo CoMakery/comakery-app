1b301436d462ead812b65770da31ae0e2cc65a67
Alex C <no-reply@wwwwww.pw>
2021-05-13 02:02:45 +0300

    Add `lockup_schedule_id` and `commencement_date` fields to transfer form

--- app/controllers/dashboard/transfers_controller.rb
+++ app/controllers/dashboard/transfers_controller.rb
@@ -106,7 +106,9 @@ class Dashboard::TransfersController < ApplicationController
         :why,
         :description,
         :requirements,
-        :transfer_type_id
+        :transfer_type_id,
+        :lockup_schedule_id,
+        :commencement_date
       )
     end
 
