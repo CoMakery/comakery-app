81a6ffc0491483cb5ea951af10e7162238d1d8d7
Zh Infuhnya <zhinfuhnya@gmail.com>
2021-05-17 21:33:09 +0300

    Update specs

--- spec/policies/project_policy_spec.rb
+++ spec/policies/project_policy_spec.rb
@@ -7,6 +7,7 @@ describe ProjectPolicy do
   let!(:project_admin) { create :account }
   let!(:project_contributor) { create :account }
   let!(:project_interested) { create :account }
+  let!(:project_observer) { create :account }
   let!(:authentication) { create(:authentication, account: project_account) }
 
   let!(:my_public_project) { create(:project, title: 'public mine', account: project_account, visibility: 'public_listed') }
@@ -36,6 +37,7 @@ describe ProjectPolicy do
       pr.admins << project_admin # TODO: this should be removed when add_admin button will be removed
       create(:award, award_type: create(:award_type, project: pr), account: project_contributor)
       create(:project_role, project: pr, account: project_interested)
+      create(:project_role, project: pr, account: project_observer, role: :observer)
       create(:project_role, project: pr, account: project_admin, role: :admin)
     end
   end
@@ -79,30 +81,47 @@ describe ProjectPolicy do
     end
   end
 
-  describe '#show_contributions? #transfers? #accounts?' do
-    %i[show_contributions? transfers? accounts?].each do |action|
+  describe '#show_contributions?' do
+    specify do
+      authorized(nil, my_public_project, :show_contributions?)
+      not_authorized(nil, my_public_project_business_confidential, :show_contributions?)
+      not_authorized(nil, my_private_project, :show_contributions?)
+    end
+
+    specify do
+      authorized(project_account, my_public_project, :show_contributions?)
+      authorized(project_account, my_public_project_business_confidential, :show_contributions?)
+      authorized(project_account, my_private_project, :show_contributions?)
+    end
+
+    specify do
+      authorized(other_team_member, my_public_project, :show_contributions?)
+      not_authorized(other_team_member, my_public_project_business_confidential, :show_contributions?)
+      authorized(other_team_member, my_private_project, :show_contributions?)
+    end
+
+    specify do
+      authorized(different_team_account, my_public_project, :show_contributions?)
+      not_authorized(different_team_account, my_public_project_business_confidential, :show_contributions?)
+      not_authorized(different_team_account, my_private_project, :show_contributions?)
+    end
+  end
+
+  describe '#transfers? #accounts?' do
+    %i[transfers? accounts?].each do |action|
       specify do
         authorized(nil, my_public_project, action)
-        not_authorized(nil, my_public_project_business_confidential, action)
         not_authorized(nil, my_private_project, action)
+        not_authorized(nil, my_public_project_business_confidential, action)
       end
 
       specify do
-        authorized(project_account, my_public_project, action)
-        authorized(project_account, my_public_project_business_confidential, action)
-        authorized(project_account, my_private_project, action)
-      end
-
-      specify do
-        authorized(other_team_member, my_public_project, action)
-        not_authorized(other_team_member, my_public_project_business_confidential, action)
-        authorized(other_team_member, my_private_project, action)
-      end
-
-      specify do
-        authorized(different_team_account, my_public_project, action)
-        not_authorized(different_team_account, my_public_project_business_confidential, action)
-        not_authorized(different_team_account, my_private_project, action)
+        authorized(project_interested, my_public_project, action)
+        authorized(project_interested, my_private_project, action)
+        authorized(project_interested, my_public_project_business_confidential, action)
+        authorized(project_observer, my_public_project_business_confidential, action)
+        authorized(project_observer, my_public_project_business_confidential, action)
+        authorized(project_observer, my_public_project_business_confidential, action)
       end
     end
   end
