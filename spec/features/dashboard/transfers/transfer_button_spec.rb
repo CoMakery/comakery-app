51021b7dc926f5fcbd031d5fe2950be9ef8a106e
olehyev138 <oleg.ye@yahoo.com>
2021-05-19 03:06:10 +0300

    failure fix

--- spec/features/dashboard/transfers/transfer_button_spec.rb
+++ spec/features/dashboard/transfers/transfer_button_spec.rb
@@ -121,7 +121,7 @@ describe 'transfer button on Transfers page' do
           end
 
           it 'links to Wallets' do
-            expect(page).to have_css('.transfers-table__transfer__status .transfer-button a', count: 1, text: 'set wallet')
+            expect(page).to have_css('.transfers-table__transfer__status .transfer-button a', count: 1, text: 'needs wallet')
           end
         end
 
@@ -179,7 +179,7 @@ describe 'transfer button on Transfers page' do
       end
 
       it 'links to blockchain transaction' do
-        expect(page).to have_css('.transfers-table__transfer__status .transfer-button ', count: 1)
+        expect(page).to have_css('.transfers-table__transfer__status .transfer-button a', count: 1)
       end
     end
 
@@ -244,7 +244,7 @@ describe 'transfer button on Transfers page' do
           end
 
           it 'links to Wallets' do
-            expect(page).to have_css('.transfers-table__transfer__status .transfer-button a', count: 1, text: 'set wallet')
+            expect(page).to have_css('.transfers-table__transfer__status .transfer-button', count: 1, text: 'needs wallet')
           end
         end
 
