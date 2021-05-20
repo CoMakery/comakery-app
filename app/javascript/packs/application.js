9b770fb63c673ae6cda12738657d32c5a4ae155a
Alex C <no-reply@wwwwww.pw>
2021-05-20 16:46:34 +0300

    Add upside styles and js into webpack
    - Add upside files in `app/javascript/src/dist`
    - Import upside files into wepback
    - Cleanup webpack
    - Fix styling conflicts in Header and Footer
    
    Known issues:
    - Wallets page has some broken styles
    - All accounts page has some broken styles
    - Modal windows close buton has broken style

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -22,8 +22,6 @@ Sentry.init({
 })
 
 require('@rails/ujs').start()
-
-import '../src/application.js'
 import '@hotwired/turbo-rails'
 
 // Support component names relative to this directory:
@@ -35,7 +33,30 @@ import 'controllers'
 
 require.context('../src/images', true)
 
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import 'animate.css'
+import '@fortawesome/fontawesome-free/scss/fontawesome.scss'
+import '@fortawesome/fontawesome-free/scss/regular.scss'
+import '@fortawesome/fontawesome-free/scss/solid.scss'
+import 'bootstrap/dist/js/bootstrap.bundle'
+
 import '../src/application.scss'
+import '../src/dist/css/upside.css'
+import '../src/dist/css/upside-vendors.css'
+import '../src/dist/js/upside.js'
+import '../src/dist/libs/apexcharts/dist/apexcharts.min.js'
+import '../src/dist/libs/choices.js/public/assets/scripts/choices.js'
+import '../src/dist/libs/countup.js/dist/countUp.js'
+import '../src/dist/libs/litepicker/dist/litepicker.js'
+import '../src/dist/libs/nouislider/distribute/nouislider.min.js'
+
+import '../src/wallets/bitcoin_trezor_script.js'
+import '../src/wallets/cardano_trezor_script.js'
+import '../src/wallets/eos_scatter_script.js'
+import '../src/wallets/qrc20_qweb3_script.js'
+import '../src/wallets/qtum_ledger_script.js'
+import '../src/wallets/tezos_trezor_script.js'
 
 ReactRailsUJS.handleEvent('turbo:load', ReactRailsUJS.handleMount)
 ReactRailsUJS.handleEvent('turbo:before-render', ReactRailsUJS.handleUnmount)
