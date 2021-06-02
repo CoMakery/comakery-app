63de1a0d796b03c268c1713002b4f054b1c0df38
Zh Infuhnya <zhinfuhnya@gmail.com>
2021-06-02 20:33:43 +0300

    Update spec

--- spec/policies/project_policy_spec.rb
+++ spec/policies/project_policy_spec.rb
@@ -144,9 +144,9 @@ describe ProjectPolicy do
     context 'when user is observer' do
       it { expect(described_class.new(project_observer, my_public_project).transfers?).to be(true) }
 
-      it { expect(described_class.new(project_observer, my_private_project).transfers?).to be(false) }
+      it { expect(described_class.new(project_observer, my_private_project).transfers?).to be(true) }
 
-      it { expect(described_class.new(project_observer, my_public_project_business_confidential).transfers?).to be(false) }
+      it { expect(described_class.new(project_observer, my_public_project_business_confidential).transfers?).to be(true) }
     end
 
     context 'without user is interested' do
