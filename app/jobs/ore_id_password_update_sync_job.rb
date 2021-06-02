97633f4cbe0101bcdaba593c8f86a01b093d4ad3
olehyev138 <oleg.ye@yahoo.com>
2021-06-03 00:59:16 +0300

    Improve test coverage for background jobs

--- app/jobs/ore_id_password_update_sync_job.rb
+++ app/jobs/ore_id_password_update_sync_job.rb
@@ -1,4 +1,4 @@
-class OreIdPasswordUpdateSyncJob < ApplicationJob
+class OreIdPasswordUpdateSyncJob < OreIdBaseJob
   queue_as :default
 
   def perform(id)
@@ -22,18 +22,6 @@ class OreIdPasswordUpdateSyncJob < ApplicationJob
       Sentry.capture_exception(e)
     else
       sync.ok!
-    end
-  end
-
-  def reschedule(ore_id)
-    self.class.set(wait: wait_to_perform(ore_id)).perform_later(ore_id.id)
-  end
-
-  def wait_to_perform(ore_id)
-    if ore_id.next_sync_allowed_after < Time.current
-      0
-    else
-      ore_id.next_sync_allowed_after - Time.current
     end
   end
 end
--- spec/jobs/ore_id_password_update_sync_job_spec.rb
+++ spec/jobs/ore_id_password_update_sync_job_spec.rb
@@ -1,45 +1,145 @@
 require 'rails_helper'
 
 RSpec.describe OreIdPasswordUpdateSyncJob, type: :job do
-  subject { create(:ore_id, skip_jobs: true, state: 'unclaimed') }
+  subject(:perform) { described_class.perform_now(ore_id.id) }
+
+  let(:now) { Time.zone.local(2021, 6, 1, 15) }
+  let(:self_reschedule_job) { double('OreIdPasswordUpdateSyncJob', perform_later: nil) }
+  let(:ore_id) { create(:ore_id, skip_jobs: true, state: 'unclaimed') }
+
+  before do
+    allow(described_class).to receive(:set).with(any_args).and_return(self_reschedule_job)
+    allow_any_instance_of(ore_id.class).to receive(:claim!)
+    allow(Sentry).to receive(:capture_exception)
+    Timecop.freeze(now)
+  end
+
+  shared_examples 'fails and reschedules itself with the delay' do |expected_delay|
+    it do
+      perform
+
+      expect(described_class).to have_received(:set).with(wait: expected_delay).once
+      expect(described_class).to have_received(:set).once
+      expect(self_reschedule_job).to have_received(:perform_later).once
+
+      expect(ore_id.synchronisations.last).to be_failed
+      expect(Sentry).to have_received(:capture_exception).with(StandardError)
+    end
+  end
+
+  shared_examples 'skips and reschedules itself with the delay' do |expected_delay|
+    it do
+      perform
+
+      expect(described_class).to have_received(:set).with(wait: expected_delay).once
+      expect(described_class).to have_received(:set).once
+      expect(self_reschedule_job).to have_received(:perform_later).once
+
+      expect(ore_id.synchronisations).to be_empty
+      expect(Sentry).not_to have_received(:capture_exception).with(StandardError)
+    end
+  end
 
   context 'when sync is allowed' do
-    before { allow_any_instance_of(subject.class).to receive(:sync_allowed?).and_return(true) }
+    before { allow_any_instance_of(ore_id.class).to receive(:sync_allowed?).and_return(true) }
 
-    it 'calls claim and sets synchronisation status to ok' do
-      expect_any_instance_of(subject.class).to receive(:claim!)
-      described_class.perform_now(subject.id)
-      expect(subject.synchronisations.last).to be_ok
+    it 'calls create_remote and sets synchronisation status to ok' do
+      expect_any_instance_of(ore_id.class).to receive(:claim!)
+
+      perform
+
+      expect(ore_id.synchronisations.last).to be_ok
     end
 
-    context 'and service raises an unknown error' do
-      before { subject.class.any_instance.stub(:service) { raise } }
+    context 'and service raises an error' do
+      context 'when error is a standard error' do
+        before { allow_any_instance_of(ore_id.class).to receive(:claim!).and_raise(StandardError) }
 
-      it 'reschedules itself and sets synchronisation status to failed' do
-        expect_any_instance_of(described_class).to receive(:reschedule)
-        expect(Sentry).to receive(:capture_exception).with(RuntimeError)
-        described_class.perform_now(subject.id)
-        expect(subject.synchronisations.last).to be_failed
+        context 'when next sync allowed time is in the future' do
+          before do
+            allow_any_instance_of(ore_id.class)
+              .to receive(:next_sync_allowed_after).and_return(1.hour.since(now))
+          end
+
+          it_behaves_like 'fails and reschedules itself with the delay', 1.hour
+        end
+
+        context 'when next sync allowed time is in the past' do
+          before do
+            allow_any_instance_of(ore_id.class)
+              .to receive(:next_sync_allowed_after).and_return(1.hour.before(now))
+          end
+
+          it_behaves_like 'fails and reschedules itself with the delay', 0
+        end
       end
-    end
 
-    context 'and service raises the OreIdAccount::ProvisioningError error' do
-      before { subject.class.any_instance.stub(:service) { raise OreIdAccount::ProvisioningError } }
+      context 'when error is a OreIdAccount::ProvisioningError' do
+        before do
+          allow_any_instance_of(ore_id.class)
+            .to receive(:claim!).and_raise(OreIdAccount::ProvisioningError)
+        end
 
-      it 'reschedules itself and sets synchronisation status to failed' do
-        expect_any_instance_of(described_class).to receive(:reschedule)
-        expect { described_class.perform_now(subject.id) }.not_to raise_error
-        expect(subject.synchronisations.last).to be_failed
+        context 'when next sync allowed time is in the future' do
+          before do
+            allow_any_instance_of(ore_id.class)
+              .to receive(:next_sync_allowed_after).and_return(1.hour.since(now))
+          end
+
+          it 'should fail and reschedule itself with the delay' do
+            perform
+
+            expect(described_class).to have_received(:set).with(wait: 1.hour).once
+            expect(described_class).to have_received(:set).once
+            expect(self_reschedule_job).to have_received(:perform_later).once
+
+            expect(ore_id.synchronisations.last).to be_failed
+            expect(Sentry).not_to have_received(:capture_exception)
+          end
+        end
+
+        context 'when next sync allowed time is in the past' do
+          before do
+            allow_any_instance_of(ore_id.class)
+              .to receive(:next_sync_allowed_after).and_return(1.hour.before(now))
+          end
+
+          it 'should fail and reschedule itself with the delay' do
+            perform
+
+            expect(described_class).to have_received(:set).with(wait: 0).once
+            expect(described_class).to have_received(:set).once
+            expect(self_reschedule_job).to have_received(:perform_later).once
+
+            expect(ore_id.synchronisations.last).to be_failed
+            expect(Sentry)
+              .not_to have_received(:capture_exception)
+              .with(instance_of(OreIdAccount::ProvisioningError))
+          end
+        end
       end
     end
   end
 
   context 'when sync is not allowed' do
-    before { allow_any_instance_of(subject.class).to receive(:sync_allowed?).and_return(false) }
+    before { allow_any_instance_of(ore_id.class).to receive(:sync_allowed?).and_return(false) }
 
-    it 'reschedules itself' do
-      expect_any_instance_of(described_class).to receive(:reschedule)
-      described_class.perform_now(subject.id)
+    context 'when next sync allowed time is in the future' do
+      before do
+        allow_any_instance_of(ore_id.class)
+          .to receive(:next_sync_allowed_after).and_return(1.hour.since(now))
+      end
+
+      it_behaves_like 'skips and reschedules itself with the delay', 1.hour
+    end
+
+    context 'when next sync allowed time is in the past' do
+      before do
+        allow_any_instance_of(ore_id.class)
+          .to receive(:next_sync_allowed_after).and_return(1.hour.before(now))
+      end
+
+      it_behaves_like 'skips and reschedules itself with the delay', 0
     end
   end
 end
