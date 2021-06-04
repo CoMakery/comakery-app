d05e57fe4687e578a37c215a3914acff9b1d5816
Zh Infuhnya <zhinfuhnya@gmail.com>
2021-06-04 18:23:20 +0300

    Reorganize transfers page styles

--- tests/EthereumBlockchain.isTransactionValid.test.js
+++ tests/EthereumBlockchain.isTransactionValid.test.js
@@ -18,6 +18,7 @@ describe("EthereumBlockchain.isTransactionValid", () => {
     expect(validResults.valid).toBe(true)
     expect(validResults.markAs).toBe(undefined)
     expect(validResults.error).toBe(undefined)
+    expect(validResults.switchHWToManualMode).toBe(undefined)
   })
 
   test('for incorrect txRaw', async () => {
@@ -28,6 +29,7 @@ describe("EthereumBlockchain.isTransactionValid", () => {
     expect(validResults.valid).toBe(false)
     expect(validResults.markAs).toBe(undefined)
     expect(validResults.error).toBe(undefined)
+    expect(validResults.switchHWToManualMode).toBe(undefined)
   })
 
   test('for not enough tokens', async () => {
@@ -37,6 +39,7 @@ describe("EthereumBlockchain.isTransactionValid", () => {
     expect(validResults.valid).toBe(false)
     expect(validResults.markAs).toBe("cancelled")
     expect(validResults.error).toEqual("The Hot Wallet has insufficient tokens. Please top up the 0x15b4eda54e7aa56e4ca4fe6c19f7bf9d82eca2fc")
+    expect(validResults.switchHWToManualMode).toBe(true)
   })
 
   test('for unknown error', async () => {
@@ -46,5 +49,6 @@ describe("EthereumBlockchain.isTransactionValid", () => {
     expect(validResults.valid).toBe(false)
     expect(validResults.markAs).toBe("failed")
     expect(validResults.error).toEqual("Unknown error: TypeError: Cannot read property 'isLessThan' of undefined")
+    expect(validResults.switchHWToManualMode).toBe(true)
   })
 });
