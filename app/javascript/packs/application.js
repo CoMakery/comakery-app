1295e19b1b1209fdd73419dd7560f99a185291df
Alex C <no-reply@wwwwww.pw>
2021-06-04 09:37:20 +0300

    Update editable and cancellable logic for transfer

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -44,8 +44,10 @@ import 'bootstrap/dist/js/bootstrap.bundle'
 import '../src/application.scss'
 import '../src/dist/css/upside.css'
 import '../src/dist/css/upside-vendors.css'
-import '../src/dist/css/tabler.min.css'
 import '../src/dist/js/upside.js'
+
+// TODO: enable when tabler migration is ready
+// import '../src/dist/css/tabler.min.css'
 
 // TODO: migrate libs to Yarn
 import '../src/dist/libs/apexcharts/dist/apexcharts.min.js'
