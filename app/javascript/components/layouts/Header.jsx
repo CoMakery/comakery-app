f82646f22c06bbf9342d61eac7d7c030166f635c
Zh Infuhnya <zhinfuhnya@gmail.com>
2021-06-08 16:41:22 +0300

    Fix menu highlighting for accounts page

--- app/javascript/components/layouts/Header.jsx
+++ app/javascript/components/layouts/Header.jsx
@@ -63,7 +63,7 @@ const navContent = (isLoggedIn, isAdmin, isWhitelabel, currentPath) => {
 
         { isAdmin &&
           <React.Fragment>
-            <a href="/accounts" className={currentPath.match(/accounts/) ? 'header--nav--links--current' : null}>
+            <a href="/accounts" className={currentPath.match(/^\/accounts$/) ? 'header--nav--links--current' : null}>
               All Accounts
             </a>
             <a href="/missions" className={currentPath.match(/missions/) ? 'header--nav--links--current' : null}>
