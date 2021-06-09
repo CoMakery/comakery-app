ed8feeda98b43e5921cc9c19c504790e57f12be8
Zh Infuhnya <zhinfuhnya@gmail.com>
2021-06-09 14:34:30 +0300

    Use clear naming

--- app/javascript/controllers/transfer_categories_controller.js
+++ app/javascript/controllers/transfer_categories_controller.js
@@ -1,16 +1,16 @@
 import { Controller } from 'stimulus'
 
 export default class extends Controller {
-  static targets = [ 'category' ];
+  static targets = [ 'transferType' ];
 
   updateTransferFormSrc() {
-    let categoryId = $(this.categoryTarget).data('categoryId');
+    let transferTypeId = $(this.transferTypeTarget).data('transferTypeId');
     let transferFormSelector = $('#transfer_form');
     let path = transferFormSelector.attr('src').split('?').shift();
 
-    transferFormSelector.attr('src', `${path}?award[transfer_type_id]=${categoryId}`);
+    transferFormSelector.attr('src', `${path}?award[transfer_type_id]=${transferTypeId}`);
 
-    // Disable lazy load of transfer form to be able to send new request with different categoryId
+    // Disable lazy load of transfer form to be able to send new request with different transferTypeId
     transferFormSelector.attr('loading', '');
   }
 }
