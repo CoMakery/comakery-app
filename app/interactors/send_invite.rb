d3ccf44e276ac55b74921a7f85d21fbc2b99d1e1
Zh Infuhnya <zhinfuhnya@gmail.com>
2021-06-10 17:35:10 +0300

    Update the flow for sending the invites

--- app/interactors/send_invite.rb
+++ app/interactors/send_invite.rb
@@ -1,21 +1,59 @@
 class SendInvite
   include Interactor
 
+  delegate :params, :whitelabel_mission, :project, to: :context
+
   def call
-    context.fail!(errors: ['The user must have signed up to add them']) if account.blank?
+    account = GetAccount.call(whitelabel_mission: whitelabel_mission, email: params[:email]).account
 
-    invite = context.project.project_roles.new(account: account, role: context.params[:role])
+    if account.blank?
+      if email_valid?
+        project_invite = project.invites.create(email: account.email, role: params[:role])
 
-    context.fail!(errors: invite.errors.full_messages) unless invite.save
+        UserMailer.send_invite_to_platform(
+          account.email,
+          project_invite.token,
+          project,
+          params[:role],
+          domain_name
+        ).deliver_now
+      else
+        context.fail!(errors: ['Email is invalid'])
+      end
+    else
+      project_role = project.project_roles.find_by(account: account)
+
+      if project_role.present?
+        context.fail!(errors: ["User already has #{project_role.role} permissions for this project. " \
+                                 "You can update their role with the action menu on this Accounts page"])
+      else
+        project_role = project.project_roles.new(account: account, role: params[:role])
+
+        if project_role.save
+          UserMailer.send_invite_to_project(
+            account.email,
+            project,
+            params[:role],
+            domain_name
+          ).deliver_now
+        else
+          context.fail!(errors: project_role.errors.full_messages)
+        end
+      end
+    end
   end
 
   private
 
-    def account
-      if context.whitelabel_mission
-        context.whitelabel_mission.managed_accounts.find_by(email: context.params[:email])
+    def email_valid?
+      params[:email] =~ URI::MailTo::EMAIL_REGEXP
+    end
+
+    def domain_name
+      if whitelabel_mission
+        whitelabel_mission.whitelabel_domain.name
       else
-        Account.find_by(email: context.params[:email], managed_mission_id: nil)
+        ENV['APP_HOST']
       end
     end
 end
