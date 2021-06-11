ffebd21214e3d8088230d46be4278b8c1df80fc0
Zh Infuhnya <zhinfuhnya@gmail.com>
2021-06-11 18:55:53 +0300

    Add GetAccount spec

---
+++ spec/interactors/get_account_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe GetAccount do
+  describe '#call' do
+    let!(:project) { create(:project) }
+    let!(:account) { create(:account) }
+
+    context 'when whitelabel mission is present' do
+      let!(:whitelabel_mission) { create(:whitelabel_mission) }
+
+      before { account.update(managed_mission: whitelabel_mission) }
+
+      subject(:result) do
+        described_class.call(whitelabel_mission: whitelabel_mission, email: email)
+      end
+
+      it { expect(result.success?).to be(true) }
+    end
+
+    context 'when whitelabel mission is nil' do
+      subject(:result) do
+        described_class.call(whitelabel_mission: nil, email: email)
+      end
+
+      it { expect(result.success?).to be(true) }
+    end
+  end
+end
