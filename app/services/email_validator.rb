29f831d3bb8a8fba9688bf2b1948876c586973a2
Zh Infuhnya <zhinfuhnya@gmail.com>
2021-06-14 23:42:16 +0300

    Add email validator service

---
+++ app/services/email_validator.rb
@@ -0,0 +1,13 @@
+class EmailValidator
+  def initialize(email)
+    @email = email
+  end
+
+  def valid?
+    email.match?(URI::MailTo::EMAIL_REGEXP)
+  end
+
+  private
+
+  attr_reader :email
+end
