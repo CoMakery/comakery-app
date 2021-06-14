9e58dafe8945fa1b5d121bac02747e236175b41c
Zh Infuhnya <zhinfuhnya@gmail.com>
2021-06-14 23:24:54 +0300

    Remove expire logic from invite

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,22 +1,9 @@
 class Invite < ApplicationRecord
-  # TODO: Clarify expiration time
-  EXPIRATION_TIME = 7.days
-
   has_secure_token
 
   belongs_to :invitable, polymorphic: true
 
   validates :email, uniqueness: { case_sensitive: false }
 
-  before_create :update_expires_at
-
   scope :pending, -> { where(accepted: false) }
-
-  def update_expires_at
-    self.expires_at = DateTime.current + EXPIRATION_TIME
-  end
-
-  def expired?
-    DateTime.current > expires_at
-  end
 end
