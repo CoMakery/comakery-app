bad7745bad94d1cc2759e7866a8256b48c611758
olehyev138 <oleg.ye@yahoo.com>
2021-06-17 00:09:41 +0300

    Improve test coverage for AwardDecorator

--- spec/factories/blockchain_transactions.rb
+++ spec/factories/blockchain_transactions.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :blockchain_transaction do
     token
+    network { :ethereum }
+    destination { '0xbbc7e3ee37977ca508f63230471d1001d22bfdd5' }
+    tx_hash { '0x5d372aec64aab2fc031b58a872fb6c5e11006c5eb703ef1dd38b4bcac2a9977d' }
   end
 end
