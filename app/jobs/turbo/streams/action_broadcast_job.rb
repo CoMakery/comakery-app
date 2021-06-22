99028450d1f91fce640a7c2b411be0e4df97a063
Alex C <no-reply@wwwwww.pw>
2021-06-22 18:52:30 +0300

    Update `Turbo::Streams::ActionBroadcastJob`
    Handle missing locals correctly

--- app/jobs/turbo/streams/action_broadcast_job.rb
+++ app/jobs/turbo/streams/action_broadcast_job.rb
@@ -3,7 +3,7 @@
 # The job that powers all the <tt>broadcast_$action_later</tt> broadcasts available in <tt>Turbo::Streams::Broadcasts</tt>.
 class Turbo::Streams::ActionBroadcastJob < ApplicationJob
   def perform(stream, action:, target:, **rendering)
-    rendering[:locals].transform_values! do |v|
+    rendering[:locals]&.transform_values! do |v|
       v.decorate
     rescue Draper::UninferrableDecoratorError, NoMethodError
       v
