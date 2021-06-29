d1c5817a818f3e725e5544eeff100feb4e4976f2
olehyev138 <oleg.ye@yahoo.com>
2021-06-30 02:08:32 +0300

    models/project coverage score improvment

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,10 +1,13 @@
 require 'rails_helper'
 require 'models/concerns/api_authorizable_spec'
-require 'models/concerns/active_storage_validator_spec'
+require 'models/concerns/has_one_attached_and_prepare_image_spec'
 
 describe Project do
   it_behaves_like 'api_authorizable'
-  it_behaves_like 'active_storage_validator', %w[image square_image panoramic_image]
+
+  it_behaves_like 'has_one_attached_and_prepare_image', 'image'
+  it_behaves_like 'has_one_attached_and_prepare_image', 'square_image'
+  it_behaves_like 'has_one_attached_and_prepare_image', 'panoramic_image'
 
   it { is_expected.to have_many(:accounts) }
   it { is_expected.to have_one_attached(:transfers_csv) }
@@ -287,45 +290,6 @@ describe Project do
         it 'is valid if url is blank' do
           project.send("#{method}=", '')
           expect(project).to be_valid
-        end
-      end
-    end
-
-    describe 'images validations' do
-      let(:project) { build(:project, image: image, square_image: image, panoramic_image: image) }
-
-      context 'file is not image' do
-        let(:image) { Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/not_img.png').to_s, 'image/png') }
-
-        it 'rejects image' do
-          expect(project.valid?).to eq(false)
-          expect(project.errors.messages[:image].last).to eq('is not a valid image')
-        end
-
-        it 'rejects panoramic_image' do
-          expect(project.valid?).to eq(false)
-          expect(project.errors.messages[:square_image].last).to eq('is not a valid image')
-        end
-
-        it 'rejects square_image' do
-          expect(project.valid?).to eq(false)
-          expect(project.errors.messages[:panoramic_image].last).to eq('is not a valid image')
-        end
-      end
-
-      context 'file is an image' do
-        let(:image) { Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/dummy_image.png').to_s, 'image/png') }
-
-        it 'accepts image' do
-          expect(project.valid?).to eq(true)
-        end
-
-        it 'accepts panoramic_image' do
-          expect(project.valid?).to eq(true)
-        end
-
-        it 'accepts square_image' do
-          expect(project.valid?).to eq(true)
         end
       end
     end
