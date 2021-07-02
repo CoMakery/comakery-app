5bb444b33dfe070f2137c295ee60f04cee6461ef
Alex C <no-reply@wwwwww.pw>
2021-07-03 01:23:45 +0300

    Add a project using security token to seeds

--- spec/factories/tokens.rb
+++ spec/factories/tokens.rb
@@ -1,11 +1,19 @@
 FactoryBot.define do
   factory :token do
     sequence(:name) { |n| "Token #{n}" }
-    denomination { :BTC }
     coin_type { :btc }
     blockchain_network { :bitcoin_mainnet }
     symbol { :BTC }
     _blockchain { :bitcoin }
     decimal_places { 0 }
+    logo_image { Rack::Test::UploadedFile.new('spec/fixtures/dummy_image.png', 'image/png') }
+
+    trait :security_token do
+      contract_address { '0x1d1592c28fff3d3e71b1d29e31147846026a0a37' }
+      _token_type { :comakery_security_token }
+      _blockchain { :ethereum_ropsten }
+      symbol { :XYZ2 }
+      decimal_places { 0 }
+    end
   end
 end
