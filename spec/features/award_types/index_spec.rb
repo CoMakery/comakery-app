24985bc46ac81f569ba5ddb45e7851fc72656acf
Zh Infuhnya <zhinfuhnya@gmail.com>
2021-07-09 12:15:37 +0300

    Update index spec

--- spec/features/award_types/index_spec.rb
+++ spec/features/award_types/index_spec.rb
@@ -1,21 +1,27 @@
 require 'rails_helper'
 
-describe 'Award Types index', skip: true do
+describe 'Award Types index' do
   context 'when project' do
     let(:project) { create :project }
 
-    before do
+    let(:visit_batches_page) do
       login(project.account)
 
       visit project_award_types_path(project)
     end
 
     context 'without mission' do
+      before { visit_batches_page }
+
       it { expect(page).to have_current_path(project_award_types_path(project)) }
     end
 
     context 'with non-whitelabel mission' do
-      before { project.update(mission: build(:mission)) }
+      before do
+        project.update(mission: build(:mission))
+
+        visit_batches_page
+      end
 
       it { expect(page).to have_current_path(project_award_types_path(project)) }
     end
@@ -23,13 +29,21 @@ describe 'Award Types index', skip: true do
     context 'with whitelabel mission and project awards hidden' do
       let(:whitelabel_mission) { create :whitelabel_mission, whitelabel_domain: 'www.example.com' }
 
-      before { project.update(mission: whitelabel_mission) }
+      before do
+        project.update(mission: whitelabel_mission)
 
-      it { expect(page).to have_current_path(root_path) }
+        visit_batches_page
+      end
+
+      it { expect(page).to have_current_path(projects_path) }
     end
 
     context 'with whitelabel mission and project awards visible' do
-      before { project.mission.update(project_awards_visible: true) }
+      before do
+        project.mission.update(project_awards_visible: true)
+
+        visit_batches_page
+      end
 
       it { expect(page).to have_current_path(project_award_types_path(project)) }
     end
