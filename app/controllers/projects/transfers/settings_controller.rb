5ea81eeeb9b9ef12b5a4bf15908d12ab9df6a7ef
Zh Infuhnya <zhinfuhnya@gmail.com>
2021-07-14 13:21:45 +0300

    Authorize show action

--- app/controllers/projects/transfers/settings_controller.rb
+++ app/controllers/projects/transfers/settings_controller.rb
@@ -1,9 +1,9 @@
 class Projects::Transfers::SettingsController < ApplicationController
-  skip_after_action :verify_authorized, :verify_policy_scoped
-
   def show
     @transfer = Award.find(params[:transfer_id])
 
     @project = @transfer.project
+
+    authorize(@project, :edit?)
   end
 end
