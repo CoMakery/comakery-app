84812835630e5e3b2029cb9b3426c9d90976a844
Alex C <no-reply@wwwwww.pw>
2021-07-15 06:43:02 +0300

    Update invitation feature specs

--- spec/acceptance/ui/whitelabel/require_invitation_spec.rb
+++ spec/acceptance/ui/whitelabel/require_invitation_spec.rb
@@ -19,10 +19,10 @@ describe 'require invitation', type: :feature, js: false do
         context 'and user has correct invite' do
           before do
             page.set_rack_session(invite_id: FactoryBot.create(:invite).id)
-            visit new_account_path
+            visit new_account_path(invited: true)
           end
 
-          it { is_expected.to have_current_path new_account_path }
+          it { is_expected.to have_current_path new_account_path(invited: true) }
         end
       end
 
@@ -39,7 +39,7 @@ describe 'require invitation', type: :feature, js: false do
           visit invite_path(FactoryBot.create(:invite).token)
         end
 
-        it { is_expected.to have_current_path new_account_path }
+        it { is_expected.to have_current_path new_account_path(invited: true) }
       end
 
       context 'when trying to unsubscribe' do
