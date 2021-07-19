7a65d200a05e9570fbf1db31cc48fadec13a1118
Sofiya Yurova <sofiya.yurova@gmail.com>
2021-07-19 19:25:01 +0300

    Dropdown toggle for closed vertical nav

--- app/javascript/controllers/vertical-navbar_controller.js
+++ app/javascript/controllers/vertical-navbar_controller.js
@@ -1,15 +1,19 @@
 import { Controller } from 'stimulus'
 
 export default class extends Controller {
-  static targets = [ 'mainSide', 'Content', 'DropdownMenu', 'Dropdown' ]
+  static targets = [ 'mainSide', 'Content', 'DropdownMenu', 'Dropdown', 'collapsedDropdown', 'collapsedFullDropdown' ]
 
   toggleVerticalNavbar() {
     if( this.mainSideTarget.classList.contains('navbar-collapsed') ) {
       this.mainSideTarget.classList.remove('navbar-collapsed')
       this.ContentTarget.classList.remove('closed-nav-vertical')
+      this.collapsedDropdownTarget.classList.add('hidden')
+      this.collapsedFullDropdownTarget.classList.remove('collapsed-full-dropdown')
     } else {
       this.mainSideTarget.classList.add('navbar-collapsed')
       this.ContentTarget.classList.add('closed-nav-vertical')
+      this.collapsedDropdownTarget.classList.remove('hidden')
+      this.collapsedFullDropdownTarget.classList.add('collapsed-full-dropdown')
     }
   }
 }
