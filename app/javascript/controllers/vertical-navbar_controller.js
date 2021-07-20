2bae8c3a6c1959fde991d944b70c74b60703d524
Sofiya Yurova <yurova@MacBook-Pro-Sofiya.local>
2021-07-20 19:22:12 +0300

    Add state to localstorage

--- app/javascript/controllers/vertical-navbar_controller.js
+++ app/javascript/controllers/vertical-navbar_controller.js
@@ -1,19 +1,74 @@
 import { Controller } from 'stimulus'
 
 export default class extends Controller {
-  static targets = [ 'mainSide', 'Content', 'DropdownMenu', 'Dropdown', 'collapsedDropdown', 'collapsedFullDropdown' ]
+  static targets = [ 'mainSide', 'Content', 'DropdownMenu', 'Dropdown', 'collapsedDropdown', 'collapsedFullDropdown', 'mainSideBody', 'mainSideToggler' ]
+
+  initialize() {
+    this.myStorage = window.localStorage;
+
+    if(this.myStorage.getItem('opened') === 'yes'){
+      this.mainSideBodyTarget.classList.remove('collapse')
+      this.mainSideBodyTarget.classList.add('show')
+      this.mainSideTogglerTarget.setAttribute('aria-expanded', 'true')
+      this.mainSideTogglerTarget.setAttribute('aria-expanded', 'true')
+      this.mainSideTarget.classList.remove('navbar-collapsed')
+      this.ContentTarget.classList.remove('closed-nav-vertical')
+      if( this.hascollapsedDropdownTarget ) {
+        this.collapsedDropdownTarget.classList.add('hidden')
+        this.collapsedFullDropdownTarget.classList.remove('collapsed-full-dropdown')
+      }
+    }
+  }
 
   toggleVerticalNavbar() {
     if( this.mainSideTarget.classList.contains('navbar-collapsed') ) {
       this.mainSideTarget.classList.remove('navbar-collapsed')
       this.ContentTarget.classList.remove('closed-nav-vertical')
-      this.collapsedDropdownTarget.classList.add('hidden')
-      this.collapsedFullDropdownTarget.classList.remove('collapsed-full-dropdown')
+      if( this.hascollapsedDropdownTarget ) {
+        this.collapsedDropdownTarget.classList.add('hidden')
+        this.collapsedFullDropdownTarget.classList.remove('collapsed-full-dropdown')
+      }
+      this.mainSideBodyTarget.classList.remove('collapse')
+      this.mainSideBodyTarget.classList.add('show')
+      this.mainSideTogglerTarget.setAttribute('aria-expanded', 'true')
+      this.myStorage.setItem('opened', 'yes')
     } else {
       this.mainSideTarget.classList.add('navbar-collapsed')
       this.ContentTarget.classList.add('closed-nav-vertical')
-      this.collapsedDropdownTarget.classList.remove('hidden')
-      this.collapsedFullDropdownTarget.classList.add('collapsed-full-dropdown')
+      this.mainSideBodyTarget.classList.add('collapse')
+      this.mainSideBodyTarget.classList.remove('show')
+      this.mainSideTogglerTarget.setAttribute('aria-expanded', 'false')
+      if( this.hascollapsedDropdownTarget ) {
+        this.collapsedDropdownTarget.classList.remove('hidden')
+        this.collapsedFullDropdownTarget.classList.add('collapsed-full-dropdown')
+      }
+      this.myStorage.setItem('opened', 'no');
+    }
+  }
+
+  toggleHorizontalNavbar() {
+    if( this.mainSideBodyTarget.classList.contains('collapse') ) {
+      this.mainSideBodyTarget.classList.remove('collapse')
+      this.mainSideBodyTarget.classList.add('show')
+      this.mainSideTogglerTarget.setAttribute('aria-expanded', 'true')
+      this.mainSideTarget.classList.remove('navbar-collapsed')
+      this.ContentTarget.classList.remove('closed-nav-vertical')
+      if( this.hascollapsedDropdownTarget ) {
+        this.collapsedDropdownTarget.classList.add('hidden')
+        this.collapsedFullDropdownTarget.classList.remove('collapsed-full-dropdown')
+      }
+      localStorage.setItem('opened', 'yes')
+    } else {
+      this.mainSideBodyTarget.classList.add('collapse')
+      this.mainSideBodyTarget.classList.remove('show')
+      this.mainSideTogglerTarget.setAttribute('aria-expanded', 'false')
+      this.mainSideTarget.classList.add('navbar-collapsed')
+      this.ContentTarget.classList.add('closed-nav-vertical')
+      if( this.hascollapsedDropdownTarget ) {
+        this.collapsedDropdownTarget.classList.remove('hidden')
+        this.collapsedFullDropdownTarget.classList.add('collapsed-full-dropdown')
+      }
+      localStorage.setItem('opened', 'no');
     }
   }
 }
